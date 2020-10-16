b0c8dc691e913141e70552c51db9d687352b3471
gitcrackerUK <pfsiwek@gmail.com>
2020-10-16 22:48:12 +0100

    Component E-Stack Card is done

--- frontend/src/components/E-Stack/card.jsx
+++ frontend/src/components/E-Stack/card.jsx
@@ -11,48 +11,60 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const useStyles = makeStyles({
     root: {
-        height:120,
-        margin:10,
-    },
-    media: {
-        width:200,
-        height:'100%',
+        margin: '10px 0px',
     },
     area: {
         display: 'flex'
     },
-    button: {
-        marginLeft: "auto !important"
+    media: {
+        display:'block',
+        margin: '0 auto',
+        minWidth: '140px',
+        maxWidth:'160px',
+        transitionDuration: '0.3s',
+        height: '130px'
     },
-    header: {
-        display: "flex"
+    body:{
+        width:"60vw"
+    },
+    header:{
+        fontSize:26,
+        fontWeight:600
+    },
+    typo: {
+        fontSize:"16px",
+        margin: '0 auto',
+        width: '100%',
     }
 
 })
-// img need to been ajusted, I cant figureout how to do it right now. Imgs are different sizes and they look bad. Option 1 is to resize them in figma or option 2 is to make them fill 100% wrapper. 
+// img need to been adjusted, I cant figure out how to do it right now. Images are different sizes and they look bad. Option 1 is to resize them in Figma or option 2 is to make them fill 100% wrapper. 
 export default function card(props) {
 
     const classes = useStyles();
     return (
         <Card className={classes.root}>
             <CardActionArea className={classes.area}>
+                <CardContent>
                     <CardMedia className={classes.media}
                         image={props.image}
                         title={props.title}
-
                     />
-                
-                <CardContent>
-                    <ButtonGroup className={classes.header}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {props.title}
-                        </Typography>
+                </CardContent>
+
+
+                <CardContent className={classes.body}>
+                    <Typography className={classes.header} gutterBottom variant="h5" component="h2">
+                        {props.title}
+                    </Typography>
+                    {/* <ButtonGroup className={classes.header}>
+                       
                         <Button variant="text" target='_blanc' href={props.href} className={classes.button} size="small" color="primary">
                             Learn More
                        </Button>
-                    </ButtonGroup>
+                    </ButtonGroup> */}
 
-                    <Typography variant="body2" color="textSecondary" component="p">
+                    <Typography className={classes.typo} variant="body2" color="textSecondary" component="p">
                         {props.text}
                     </Typography>
                 </CardContent>
