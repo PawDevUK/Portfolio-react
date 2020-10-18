477f2bbd1d2a7776806cab46a65b13dda0e019fa
gitcrackerUK <pfsiwek@gmail.com>
2020-10-18 11:58:44 +0100

    reduced card width and added box shadow for cardSolo. Added aos effect for main-hello

--- frontend/src/components/B-Top-picture/top-intro.jsx
+++ frontend/src/components/B-Top-picture/top-intro.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import '../../style.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 export default class TopIntro extends Component {
+
+    componentDidMount(){
+        AOS.init({duration:1000});
+    }
+
+
     render() {
         return (
             <section id="home">
             <div className="container">
                 <div className="row justify-content-sm-center">
                     <div className="col-sm-8 col-lg-10">
-                        <div className="main-hello">
+                        <div data-aos="fade-right" className="main-hello">
                             <h2 className="" id="hallo"><strong>Hello</strong></h2>
                             <h5 className=" ">I'm <strong className="text-blue">PAWEL SIWEK</strong>, a web developer. I'm happy to help you with your Website or Web App.</h5>
                         </div>
--- frontend/src/components/D-Projects/projects.jsx
+++ frontend/src/components/D-Projects/projects.jsx
@@ -25,12 +25,12 @@ export default class Projects extends Component {
     }
     render() {
         return (
-            <div >
-                 <h2 >Projects</h2>
+            <div>
+                 <h2 className="mt-3">Projects</h2>
                  <div id="projects" className="mt-3 jumbo d-flex flex-wrap">
                 {this.state.cards.map((item, index) => {
                     return (
-                        <div  data-aos="fade" key={index} className="card col-xs-10 cardSolo mx-auto my-1">
+                        <div  data-aos="fade" key={index} className="card col-xs-10 cardSolo mx-auto my-2">
                             <img className="card-img-top mt-0" width="100%" src={item.src} alt={item.alt}></img>
                             <div className="card-body text-center">
                                 <h5 className="card-title">{item.title}</h5>
@@ -42,8 +42,6 @@ export default class Projects extends Component {
                     )
                 })
                 }
-             
-                
             </div>
 
             </div>
