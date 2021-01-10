71085c9a8a0fd85c512ac94de91dc32960710f2c
gitcrackerUK <pfsiwek@gmail.com>
2021-01-10 10:37:30 +0000

    moved year to separate component

--- frontend/src/components/F-Footer/footer.jsx
+++ frontend/src/components/F-Footer/footer.jsx
@@ -1,68 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
-import { makeStyles } from '@material-ui/core/styles';
 import Particles from 'react-particles-js'
 import Background from '../../img/BackgroundBlue.png'
-import FooterCurrentYear from './FooterYear'
+import FooterYear from './FooterYear'
 
-const logoBlue = "#1598C3"
-const backgroundColor = '#35495e'
+// const logoBlue = "#1598C3"
+// const backgroundColor = '#35495e'
 
-const Year = styled(FooterCurrentYear)`
-                 color:red;
-                 font-size:10px;
-                `
+const Wrapper = styled.div`
+position:relative;
+padding:0px;
+margin:0px;
+height: 200px;
+width: 100%;
+background-image: url(${Background});
+background-repeat: no-repeat;
+background-size: 100%;
+`
+const StParticles = styled(Particles)`
+padding: 0px;
+margin: 0px;
+height: 100%;
+width:100%;
+`
 
-
-const useStyles = makeStyles({
-    root: {
-        padding:0,
-        margin:0,
-        height: 200,
-        width: "100%",
-        backgroundImage: `url(${Background})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100%'
-    },
-    particles: {
-        padding:0,
-        margin:0,
-        height: "100%"
-    }
-})
+const Year = styled(FooterYear)`
+position:absolute;
+top:0px;
+color:red;
+font-size:10px;
+`
 
 export default function Footer() {
-
-
-    const classes = useStyles();
-
-    
     return (
-        <div className={classes.root}>
-            <Particles className={classes.particles}
-                params={{
-                    particles: {
-                        number: {
-                            value: 20
-                        },
-                        size: {
-                            value: 1
-                        },
-                        autoPlay: true,
+        <Wrapper>
+            <Year></Year>
+        </Wrapper>
 
-                        opacity: {
-                            value: 0.3,
-                            random: true
-                        },
-                        links: {
-                            enable: true,
-                            opacity: 0.1
-                        },
 
-                    }
-
-                }} />
-            {/* <Year></Year> */}
-        </div>
     )
 }
