46769d7dec9d2427f17bac588540f4ec12fb079c
gitcrackerUK <pfsiwek@gmail.com>
2021-01-13 22:45:38 +0000

    Changed component name from  "what -I-do"

---
+++ frontend/src/components/WhatIUse/whatIUse.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import 'style.css'
+
+
+export default class WhatIDO extends Component {
+    render() {
+        return (
+            <div >
+            
+                <h2>What I Do</h2>
+            
+            <div >
+                <div className="card">
+                    <div className="card-body">
+                        <p id="about-header-text" className="col-12 col-md-10 p-3 text-center m-auto">
+                            Everyone needs something different, my main goal is to understand every customer so I can deliver
+                            right content.
+                            I can create modern looking website with ease to read content.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
