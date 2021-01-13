7102611877e5004048bb2e4d8b38d57c30d10d5e
gitcrackerUK <pfsiwek@gmail.com>
2021-01-13 22:44:00 +0000

    trimed footer, without old background img

--- frontend/src/components/F-Footer/footer.jsx
+++ frontend/src/components/F-Footer/footer.jsx
@@ -1,34 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
-import Particles from 'react-particles-js'
-import Background from '../../img/BackgroundBlue.png'
 import FooterYear from './FooterYear'
 
-// const logoBlue = "#1598C3"
-// const backgroundColor = '#35495e'
 
 const Wrapper = styled.div`
-position:relative;
+display:flex;
+justify-content:flex-end;
 padding:0px;
 margin:0px;
 height: 200px;
-width: 100%;
-background-image: url(${Background});
-background-repeat: no-repeat;
-background-size: 100%;
-`
-const StParticles = styled(Particles)`
-padding: 0px;
-margin: 0px;
-height: 100%;
-width:100%;
-`
 
+background-color: #17293f;
+`
 const Year = styled(FooterYear)`
-position:absolute;
-top:0px;
-color:red;
-font-size:10px;
+font-size:50px;
 `
 
 export default function Footer() {
