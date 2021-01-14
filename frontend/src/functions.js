c0f5ed106a7c332a7cc0d701363c2195e1177532
gitcrackerUK <pfsiwek@gmail.com>
2021-01-14 21:18:16 +0000

    added functions.js and useWindowWidth

---
+++ frontend/src/functions.js
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react'
+
+export function useWindowWidth() {
+    const [width, setWidth] = useState(window.innerWidth);
+    const handleResize = () => setWidth(window.innerWidth);
+    useEffect(() => {
+      
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    });
+    
+    return width;
+  }
