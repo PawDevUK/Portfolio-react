8403b2755ef2ffe3b71221993a97122bb501b614
gitcrackerUK <pfsiwek@gmail.com>
2021-01-14 21:09:04 +0000

    added 100px width and color for sectionHeader

--- frontend/src/components/common/SectionHeader.js
+++ frontend/src/components/common/SectionHeader.js
@@ -2,9 +2,11 @@ import styled from 'styled-components'
 
 
 const SectionHeader = styled.h6`
+color: rgba(0, 0, 0, 0.7);
 font-size:20px;
 height:30px;
-width:20%;
+width:100px;
+text-transform:capitalize;
 `
 
 export default SectionHeader
