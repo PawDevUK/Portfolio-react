fac06be7e5b5a283e935133030671559c403867d
gitcrackerUK <pfsiwek@gmail.com>
2021-01-14 21:07:29 +0000

    exported img to store

--- frontend/src/img/index.js
+++ frontend/src/img/index.js
@@ -4,3 +4,13 @@ export {default as royal} from './royal.png';
 export {default as barber} from './barber.jpg';
 export {default as solicitors} from './solicitors.png';
 export {default as fitnessApp} from './FitnessApp.png';
+
+export { default as react} from './React.png'
+export { default as reactSvg} from './react.svg'
+export { default as git} from './github.png'
+export { default as mongo} from './mongoDb.png'
+export { default as nodeJS} from './node.jpg'
+export { default as material} from './materialUI.png'
+export { default as gcloud} from './gcloud.png'
+export { default as bootstrap} from './bootstrap.png'
+export { default as figma} from './figma.png'
---
+++ frontend/src/store.js
@@ -0,0 +1,79 @@
+import { react, git, mongo, nodeJS, material, gcloud, bootstrap, figma } from 'img'
+
+
+
+export const Menu = [{
+    title: "Projects",
+    href: "#home",
+    style: "Green",
+    disabled: false
+},
+{
+    title: "About",
+    href: "#about",
+    disabled: false,
+
+},
+{
+    title: "What I Use",
+    href: "#use",
+    disabled: false
+},
+{
+    title: "React",
+    href: "#react",
+    disabled: false,
+},
+{
+    title: "Hacker News",
+    href: "#hacker",
+    disabled: false,
+}]
+
+export const stack = [
+    {
+        img: react,
+        title: "React",
+        text: "React makes it painless to create interactive UIs. Design simple views...",
+        webHref: "https://reactjs.org/",
+    }, {
+        img: bootstrap,
+        title: "Bootstarp",
+        text: "Quickly design and customize responsive mobile-first sites with Bootst...",
+        webHref: "https://getbootstrap.com",
+    }, {
+        img: material,
+        title: "Material-UI",
+        text: "Material-UI is an open-source project that features React components t...",
+        webHref: "https://material-ui.com/",
+    }, {
+        img: figma,
+        title: "Figma",
+        text: "Figma is a browser-based UI and UX design application, with excellent ...",
+        webHref: "https://www.figma.com/",
+    }, {
+        img: gcloud,
+        title: "Google Cloud",
+        text: "Google Cloud Platform is a suite of public cloud computing services of...",
+        webHref: "https://cloud.google.com/",
+    }, {
+        img: nodeJS,
+        title: "NodeJs",
+        text: "Node.Js (Node) is an open source development platform for executing Ja...",
+        webHref: "https://nodejs.org/en/",
+    }, {
+        img: mongo,
+        title: "MongoDB",
+        text: "MongoDB is a cross-platform and open-source document-oriented database...",
+        webHref: "https://www.mongodb.com/",
+    }, {
+        img: git,
+        title: "GitHub",
+        text: "GitHub is a Git repository hosting service, but it adds many of its ow...",
+        webHref: "https://github.com/",
+    }
+
+
+
+
+]
