c6d0986cb908251490cfdd9646632a7d89a38b59
gitcrackerUK <pfsiwek@gmail.com>
2021-01-18 11:15:34 +0000

    moved card to separate component, add JumboSection

--- frontend/src/components/F-Projects/projects.jsx
+++ frontend/src/components/F-Projects/projects.jsx
@@ -1,36 +1,37 @@
 
-import {projects} from 'store'
-import React, { useEffect } from 'react'
+import JumboSectionHeader from 'components/common/JumboSectionHeader'
 import '../../../node_modules/aos/dist/aos.css';
+import styled from 'styled-components'
+import React, { useEffect } from 'react'
+import {projects} from 'store'
 import AOS from 'aos';
+import Card from  './card'
+
+
+const Wrapper = styled.div`
+display:flex;
+flex-wrap:wrap;
+`
 
 
 export default function Projects() {
   
-
     useEffect(() => {
         AOS.init({ duration: 1000 });
     }, [])
 
     return (
         <div>
-            <h2 className="mt-3">Projects</h2>
-            <div id="projects" className="mt-3 jumbo d-flex flex-wrap">
+           <JumboSectionHeader>Projects</JumboSectionHeader>
+            <Wrapper id="projects" >
+            
                 {projects.map((item, index) => {
                     return (
-                        <div data-aos="fade" key={index} className="card col-xs-10 cardSolo mx-auto my-2">
-                            <img className="card-img-top mt-0" width="100%" src={item.src} alt={item.alt}></img>
-                            <div className="card-body text-center">
-                                <h5 className="card-title">{item.title}</h5>
-                                <p className="card-text">{item.text}</p>
-                                <a href={item.webHref.href} target="_blank" className="btn btn-outline-primary">{item.webHref.button}</a>
-                                <a href={item.githubHref} target="_blank" className="btn btn-dark m-1">Github Repo</a>
-                            </div>
-                        </div>
+                       <Card item={item}></Card>
                     )
                 })
                 }
-            </div>
+            </Wrapper>
         </div>
     )
 
