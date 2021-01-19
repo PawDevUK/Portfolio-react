36a178e81c6e882068a633c06487a9aca879263d
gitcrackerUK <pfsiwek@gmail.com>
2021-01-19 10:49:50 +0000

    added margin to paragraphs

--- frontend/src/components/C-About/top-intro.jsx
+++ frontend/src/components/C-About/top-intro.jsx
@@ -15,7 +15,10 @@ const Card = styled.div`
 background: #fff;
 width:100%;
 `
+const StP = styled(P)`
+margin:10px;
 
+`
 
 
 export default function TopIntro() {
@@ -23,22 +26,21 @@ export default function TopIntro() {
     useEffect(() => AOS.init({ duration: 1000 }))
 
     return (
-        <Wrapper>
+        <Wrapper id="about">
             <Card data-aos="fade-right" >
                 <JumboSectionHeader>About Me</JumboSectionHeader>
-                <P>
-                    My name is <Bold>Pawel</Bold>, I'm self thought and self motivated programmer from Reading UK. <br />
-                    I started from
-                    jumping without prior knowledge into programming back in 2018 and now I'm <Bold>React Developer</Bold>.</P>
-                <P>
-                    I can design from scratch and create, modern looking websites and web application with functionality including <Bold>API</Bold>. To design I use <Bold>Figma</Bold> which helps me to make the modern looking layouts with the use of number of popular design patterns. Currently most of the users browse web on mobile devices so <Bold>RWD</Bold> is quite important that's why I'm implementing it in my websites and applications.
-                </P>
-                <P>
-                    I currently use  <Bold>React</Bold> which requires good knowledge of number of technologies like <Bold>JavaScript ES6</Bold>, HTML, CSS, Package Manager,  WebPack, Babel and many more. Most of my work is kept via Git CLI on <Bold>GitHub</Bold> so I branch, commit, push, pull and merge fairly often. </P>
-                <P>
+                <StP>
+                    My name is <Bold>Pawel</Bold>, I am a self-thought and self-motivated programmer from Reading, UK. <br />
+                    I started programming without any prior knowledge back in 2018 and now I'm proficient <Bold>React Developer</Bold>.</StP>
+                <StP>
+                    I design and create, modern looking websites and web applications with functionality including <Bold>API</Bold>. I use <Bold>Figma</Bold> to helps me with design of modern looking layouts with the use of number of popular design patterns. Currently, most of the internet users browse Web on mobile devices therefor <Bold>RWD</Bold> is quite important that is why I am implementing it in my websites and applications.
+                </StP>
+                <StP>
+                    I currently use  <Bold>React</Bold> which requires good knowledge of number of technologies like <Bold>JavaScript ES6</Bold>, HTML, CSS, Package Manager,  WebPack, Babel and many more. Most of my work is kept via Git CLI on <Bold>GitHub</Bold> so I branch, commit, push, pull and merge fairly often. </StP>
+                <StP>
                     All my work is done on <Bold>Linux</Bold> machine so Terminal is used on a regular basis. Code editor of my choice is <Bold>Visual Studio Code</Bold>. Also I know how to build a backend for simple communication(express.js) with frontend(Axios) and database. <br />
-                    <Bold>MERN</Bold> stack is not what I specialize in at the moment but I would like to master it in the nearest future.
-                </P>
+                    The next step for me will be <Bold>MERN</Bold> stack which I am planning to master in the nearest future.
+                </StP>
             </Card>
 
 
