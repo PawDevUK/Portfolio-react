3226c4a00295dd3b483d5c7d91b59efb768d45aa
gitcrackerUK <pfsiwek@gmail.com>
2021-01-23 09:52:05 +0000

    removed Year to separate function in factory

--- src/components/G-Footer/FooterYear.jsx
+++ src/components/G-Footer/FooterYear.jsx
@@ -1,17 +1,13 @@
 import React from "react"
 import styled from 'styled-components'
+import {Year} from 'factory'
 
 const Wrapper = styled.div`
 color:white;
 width:100vw;
 `
-
 export default function FooterYear() {
-
-    function year(){
-        return new Date().getFullYear()
-    }
     return (
-        <Wrapper>{year()}</Wrapper>
+        <Wrapper>{Year()}</Wrapper>
     )
 }
