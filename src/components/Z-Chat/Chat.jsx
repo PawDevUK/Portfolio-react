313f9cf01cd694327256588ff29ee0b6816f1a24
gitcrackerUK <pfsiwek@gmail.com>
2021-01-25 07:39:17 +0000

    Added "open" boleen to togle window open/close

--- src/components/Z-Chat/Chat.jsx
+++ src/components/Z-Chat/Chat.jsx
@@ -1,28 +1,38 @@
-import ChatWindow from 'img/BotChatWindow.svg'
+import ChatButtonLarge1px from 'img/ChatButtonLarge1px.svg'
 import { SvgLoader } from 'react-svgmt'
-import React from "react"
-import styled from 'styled-components'
+import React,{useState} from "react"
+import styled,{css} from 'styled-components'
 
-const Window = styled(SvgLoader)`
-
+const Button = styled(SvgLoader)`
+padding:0px;
+margin:0px;
 `
 const Wrapper = styled.div`
+/* border:1px solid black; */
+/* ${p=>console.log(p)} */
+display:block;
+transform:right 1s;
 @media(max-width:1000px){
 display:none;
 }
-display:block;
+${p=>p.open?css`
 position:absolute;
-top:400px;
-right:-257px;
+top:300px;
+right:250px;
+`:css`
+position:absolute;
+top:300px;
+right:0px;`}
 `
 
 export default function Chat({...props}){
+    const [open, setOpen]= useState(false)
     function HandleClick(){
-        console.log("click");
+        setOpen(p=>p=!p)
     }
     return(
-        <Wrapper onClick={HandleClick} >
-            <Window path={ChatWindow}></Window>
+        <Wrapper open={open} onClick={HandleClick}>
+            <Button path={ChatButtonLarge1px}></Button>
         </Wrapper>
     )
 }
