844295c2206570df9024a51fe6172027c090590c
gitcrackerUK <pfsiwek@gmail.com>
2021-01-26 19:41:09 +0000

    aded window to chat component and align it

--- src/components/Z-Chat/Chat.jsx
+++ src/components/Z-Chat/Chat.jsx
@@ -8,31 +8,38 @@ padding:0px;
 margin:0px;
 `
 const Wrapper = styled.div`
-/* border:1px solid black; */
-/* ${p=>console.log(p)} */
-display:block;
-transform:right 1s;
+display:flex;
+/* transform:right 1s; */
 @media(max-width:1000px){
 display:none;
 }
 ${p=>p.open?css`
 position:absolute;
 top:300px;
-right:250px;
+right:0px;
 `:css`
 position:absolute;
 top:300px;
 right:0px;`}
 `
 
+const Window = styled.div`
+${p=>console.log(p)}
+${p=>p.open?css`display:block;`:css`display:none;`};
+width:250px;
+height:400px;
+background-color:white;
+`
+
 export default function Chat({...props}){
-    const [open, setOpen]= useState(false)
+    const [open, setOpen]= useState(true)
     function HandleClick(){
         setOpen(p=>p=!p)
     }
     return(
         <Wrapper open={open} onClick={HandleClick}>
             <Button path={ChatButtonLarge1px}></Button>
+            <Window open={open}></Window>
         </Wrapper>
     )
 }
