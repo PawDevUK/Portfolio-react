a8c4abc7d8a4b2ff89f5572b0bfee5a29ebb9d19
gitcrackerUK <pfsiwek@gmail.com>
2021-02-02 09:14:28 +0000

    update text

--- src/components/C-About/top-intro.jsx
+++ src/components/C-About/top-intro.jsx
@@ -36,7 +36,7 @@ export default function TopIntro() {
                     I design and create, modern looking websites and web applications with functionality including <Bold>API</Bold>. I use <Bold>Figma</Bold> to helps me with design of modern looking layouts with the use of number of popular design patterns. Currently, most of the internet users browse Web on mobile devices therefor <Bold>RWD</Bold> is quite important that is why I am implementing it in my websites and applications.
                 </StP>
                 <StP>
-                    I currently use  <Bold>React</Bold> which requires good knowledge of number of technologies like <Bold>JavaScript ES6</Bold>, HTML, CSS, Package Manager,  WebPack, Babel and many more. Most of my work is kept via Git CLI on <Bold>GitHub</Bold> so I branch, commit, push, pull and merge fairly often. </StP>
+                    I use  <Bold>React</Bold> which requires good knowledge of number of technologies like <Bold>JavaScript ES6</Bold>, HTML, CSS, Package Manager,  WebPack, Babel and many more. Most of my work is kept via Git CLI on <Bold>GitHub</Bold> so I branch, commit, push, pull and merge fairly often. </StP>
                 <StP>
                     All my work is done on <Bold>Linux</Bold> machine so Terminal is used on a regular basis. Code editor of my choice is <Bold>Visual Studio Code</Bold>. Also I know how to build a backend for simple communication(express.js) with frontend(Axios) and database. <br />
                     The next step for me will be <Bold>MERN</Bold> stack which I am planning to master in the nearest future.
