fa0b98645202c69e7da0524ca491fb75aaaac12e
gitcrackerUK <pfsiwek@gmail.com>
2021-02-03 17:53:15 +0000

    reusable css class with use of styled css helper

---
+++ src/styled.js
@@ -0,0 +1,10 @@
+import { css } from 'styled-components'
+
+export const boxShadow12 = css`
+box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);
+`
+export const ImgCover = css`
+background-size: cover;
+background-position: top center; 
+background-repeat:no-repeat;
+`
