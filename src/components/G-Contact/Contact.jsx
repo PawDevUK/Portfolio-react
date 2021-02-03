f62c6da4241d0d2874cbde32761a7e85431febce
gitcrackerUK <pfsiwek@gmail.com>
2021-02-03 23:29:21 +0000

    Lots of changes in Contact section & JumboHeader

--- src/components/G-Contact/Contact.jsx
+++ src/components/G-Contact/Contact.jsx
@@ -1,64 +1,50 @@
 import JumboSectionHeader from 'components/common/JumboSectionHeader';
-import { boxShadow12,ImgCover } from 'styled.js'
+import {P,Bold} from 'components/common/typography'
+import { boxShadow12, ImgCover } from 'styled.js'
 import styled from 'styled-components';
-import { MyPhoto } from 'img'
+import { MyPhoto, SkyScraper } from 'img'
 import React from 'react';
 
 
 const Wrapper = styled.div`
-background-image: url(${MyPhoto});
+background-image: url(${SkyScraper});
+${ImgCover} // properties for parallax background
 position:relative;
-height:700px;
+height:500px;
 width:100%;
-${ImgCover}
 `
 const OuterTextWrapper = styled.div`
 border-radius:5px;
 position:absolute;
-${boxShadow12}
-top:400px;
-left:5vw;
-right:5vw;
-`
-const TextWrapper = styled.div`
-position:relative;
-width:100%;
-`
-const BlueBox = styled.div`
-padding:0px 0px 10px 8px;
-background-color:#17293F;
-border-radius:3px;
-position:absolute;
-${boxShadow12}
-width:280px;
-left:50px;
-top:-50px;
+top:100px;
+left:10vw;
+right:10vw;
 `
 const WhiteBox = styled.div`
+${boxShadow12}
 border-radius:5px;
 background-color:#fff;
 width:100%;
-height:270px;
+padding:10px;
+`
+const TextWrapper = styled.div`
+margin:20px 0px 0px 10px;
 `
 
 export default function Contact() {
     return (
         <Wrapper>
             <OuterTextWrapper>
-                <TextWrapper>
-                    <BlueBox>
-                        <JumboSectionHeader dark >
-                            Contact
-                        </JumboSectionHeader>
-                    </BlueBox>
-                    <boxShadow12>
-                        <WhiteBox>
-
-
-                        </WhiteBox>
-                    </boxShadow12>
-
-                </TextWrapper>
+                <WhiteBox>
+                    <JumboSectionHeader>
+                        Contact
+                    </JumboSectionHeader>
+                    <TextWrapper>
+                        <P>If you would like to contact me regarding any queries please don't  <br/> hesitate and drop me an email  or call me directly on my mobile. <br/> I'm always more than happy to speak and answer any questions.</P> <br/>
+                        <P><Bold>Email:</Bold> p.f.siwek@gmail.com<br/>
+                        <Bold>Mobile:</Bold> 07463765514</P>
+                    </TextWrapper>
+                </WhiteBox>
             </OuterTextWrapper>
         </Wrapper>
     )
--- src/components/common/JumboSectionHeader.jsx
+++ src/components/common/JumboSectionHeader.jsx
@@ -1,43 +1,33 @@
-import React, {useEffect} from 'react'
-import {SectionHeader,JumboHeader } from './typography'
+import React, { useEffect } from 'react'
+import { SectionHeader, JumboHeader } from './typography'
 import styled from 'styled-components'
 import 'aos/dist/aos.css';
 import AOS from 'aos';
 
 const Wrapper = styled.div`
-@media(max-width:992px){
-    height:80px;
-    position:relative;
-}
-height:100px;
+height:60px;
 position:relative;
 margin:5px;
 `
 const Header = styled(SectionHeader)`
-
 @media(max-width:992px){
-    margin:0px;
-    padding:0px;
     position:absolute;
     left:7px;
-    top:30px;
+    top:20px;
 }
-margin:0px;
-padding:0px;
 position:absolute;
 left:7px;
 top:30px;
 `
 const Jumbo = styled(JumboHeader)`
 @media(max-width:992px){
-    opacity:0.5;
     font-size:50px;
-    height:60px;
     margin:0px;
     padding:0px;
     position:absolute;
     top:0px;
 }
+height:60px;
 position:absolute;
 top:0px;
 `
@@ -48,8 +38,10 @@ export default function JumboSectionHeader({ ...props }) {
 
     return (
         <Wrapper>
-            <Jumbo  {...props}data-aos="zoom-in">{props.children}</Jumbo>
-            <Header {...props} >{props.children}</Header>
+            <div>
+                <Jumbo  {...props} data-aos="zoom-in">{props.children}</Jumbo>
+                <Header {...props} >{props.children}</Header>
+            </div>
         </Wrapper>
 
 
