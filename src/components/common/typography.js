f0948f6a844dfad9a17f2c5ca978e0ba92cb7dd6
gitcrackerUK <pfsiwek@gmail.com>
2021-02-03 17:54:13 +0000

    reduced opacity for JumboHeader

--- src/components/common/typography.js
+++ src/components/common/typography.js
@@ -5,7 +5,7 @@ text-transform:capitalize;
 font-weight:600;
 font-size:70px;
 height:100px;
-color:rgba(0, 0, 0, 0.1);
+color:rgba(0, 0, 0, 0.08);
 ${p=>p.dark?css`color:rgba(41, 87, 111, 0.46);`:css``}
 `
 
