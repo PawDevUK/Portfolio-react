be4be9f407c05c01ea1c0d45c0c4ad8bed7e914d
gitcrackerUK <pfsiwek@gmail.com>
2021-02-14 19:14:37 +0000

    added contact section with form, need backend

--- src/components/G-Contact/Contact.jsx
+++ src/components/G-Contact/Contact.jsx
@@ -1,5 +1,8 @@
 import JumboSectionHeader from 'components/common/JumboSectionHeader';
+import TextField from '@material-ui/core/TextField';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import {P,Bold} from 'components/common/typography'
+import Button from 'components/common/Button'
 import { boxShadow12, ImgCover } from 'styled.js'
 import styled from 'styled-components';
 import {SkyScraper } from 'img'
@@ -9,43 +12,82 @@ import React from 'react';
 const Wrapper = styled.div`
 background-image: url(${SkyScraper});
 ${ImgCover} // properties for parallax background
-position:relative;
-height:500px;
+/* height:1000px; */
 width:100%;
+padding:1px;
 `
-const OuterTextWrapper = styled.div`
-border-radius:5px;
-position:absolute;
-right:10vw;
-top:100px;
-left:10vw;
-`
+
 const WhiteBox = styled.div`
+margin:100px auto;
 background-color:#fff;
 border-radius:5px;
 ${boxShadow12}
 padding:10px;
-width:100%;
+width:80%;
+`
+const InnerWrapper = styled.div`
+width:540px;
+margin:auto;
 `
 const TextWrapper = styled.div`
-margin:20px 0px 0px 10px;
+margin:0px auto;
+`
+const Form = styled.form`
+margin:auto;
+`
+const ButtonWrapper = styled.div`
+width:250px;
+display:flex;
+justify-content:space-between;
+height:100px;
+margin:5px auto;
+`
+const StTextField = styled(TextField)`
+margin:10px!important;
+width:250px;
+`
+const StTextareaAutosize = styled(TextareaAutosize)`
+display:block;
+width:90%;
+margin:20px auto !important;
+:active{
+
+}
 `
 
 export default function Contact() {
     return (
         <Wrapper>
-            <OuterTextWrapper>
                 <WhiteBox>
+                    <InnerWrapper>
                     <JumboSectionHeader>
                         Contact
                     </JumboSectionHeader>
                     <TextWrapper>
                         <P>If you would like to contact me regarding any queries please don't  <br/> hesitate and drop me an email  or call me directly on my mobile. <br/> I'm always more than happy to speak and answer any questions.</P> <br/>
-                        <P><Bold>Email:</Bold> p.f.siwek@gmail.com<br/>
-                        <Bold>Mobile:</Bold> 07463765514</P>
+                        <P style={{display:"flex"}}>
+                             <Bold>Email:</Bold> p.f.siwek@gmail.com<br/>
+                             <Bold>Mobile:</Bold> 07463765514
+                        </P>
                     </TextWrapper>
+                            <Form  noValidate autoComplete="off">
+                                <StTextField required id="standard-basic" label="First Name" />
+                                <StTextField id="standard-basic" label="Last Name" />
+                                <StTextField  required id="standard-basic" label="Email" />
+                                <StTextField id="standard-basic" label="Phone" />
+                                <StTextField id="standard-basic" label="Website" />
+                                <StTextField id="standard-basic" label="Company" />
+                                <StTextareaAutosize required aria-label="minimum height" rowsMin={4} required id="outlined-basic" placeholder="Message" variant="outlined"/>
+                                <ButtonWrapper>
+                                     <Button>Send</Button>
+                                     <Button>Clear</Button>
+                                </ButtonWrapper>
+                            </Form>
+                            <TextWrapper>
+                    </TextWrapper>
+                    </InnerWrapper>
+                   
                 </WhiteBox>
-            </OuterTextWrapper>
         </Wrapper>
     )
 }
