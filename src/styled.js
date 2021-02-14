14387102b2884351c92d4c8f40e51492c6dbfd43
gitcrackerUK <pfsiwek@gmail.com>
2021-02-14 19:15:36 +0000

    added boxShadow6,8,12 and global BackgroundBlue

--- src/styled.js
+++ src/styled.js
@@ -1,8 +1,16 @@
 import { css } from 'styled-components'
 
-export const boxShadow12 = css`
-box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);
+export const BackgroundBlue = css`
+background-color:#17293F;
 `
+
+export const boxShadow6 = css`box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.14), 0px 1px 18px rgba(0, 0, 0, 0.12), 0px 3px 5px rgba(0, 0, 0, 0.2);
+border-radius:3px;`
+export const boxShadow8 = css`box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.14), 0px 3px 14px rgba(0, 0, 0, 0.12), 0px 5px 5px rgba(0, 0, 0, 0.2);`
+export const boxShadow12 = css`box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);`
+
+
+
 export const ImgCover = css`
 background-size: cover;
 background-position: top center; 
