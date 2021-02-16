00314cf1541b3e813784ad9b1be7ceff42a50075
gitcrackerUK <pfsiwek@gmail.com>
2021-02-16 10:13:34 +0000

    Added form to contact me via node nodemailer

--- src/components/G-Contact/Contact.jsx
+++ src/components/G-Contact/Contact.jsx
@@ -3,10 +3,11 @@ import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import TextField from '@material-ui/core/TextField';
 import {P,Bold} from 'components/common/typography'
 import { boxShadow12, ImgCover } from 'styled.js'
+import {SendFormData} from 'RESTapi/contactForm'
 import Button from 'components/common/Button'
 import styled from 'styled-components';
+import React,{useState}from 'react';
 import {SkyScraper } from 'img'
-import React from 'react';
 
 const Wrapper = styled.div`
 background-image: url(${SkyScraper});
@@ -15,9 +16,9 @@ width:100%;
 padding:1px;
 `
 const WhiteBox = styled.div`
-margin:100px auto;
 background-color:#fff;
 border-radius:5px;
+margin:100px auto;
 ${boxShadow12}
 padding:10px;
 width:80%;
@@ -36,11 +37,12 @@ const Form = styled.form`
 margin:auto;
 `
 const ButtonWrapper = styled.div`
-width:250px;
-display:flex;
 justify-content:space-between;
-height:100px;
 margin:5px auto;
+display:flex;
+height:100px;
+width:250px;
+
 `
 const StTextField = styled(TextField)`
 margin:10px!important;
@@ -52,6 +54,41 @@ width:90%;
 margin:20px auto !important;
 `
 export default function Contact() {
+    let [formData, setFormData] = useState({
+       FirstName:'',
+       LastName:'',
+       Email:'',
+       Phone:'',
+       Website:'',
+       Company:'',
+       Message:'',
+    });
+
+    function handleChange(e){
+        const {name,value} = e.target;
+       setFormData(prev=>({
+           ...prev,
+           [name]:value
+       }))
+    }
+    function handleSubmit(){
+        SendFormData.post('./contactForm',formData)
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err))
+        handleClear()
+    }
+
+    function handleClear(){
+        setFormData({
+            FirstName:'',
+            LastName:'',
+            Email:'',
+            Phone:'',
+            Website:'',
+            Company:'',
+            Message:'',
+        })
+    }
     return (
         <Wrapper>
                 <WhiteBox>
@@ -76,17 +113,17 @@ export default function Contact() {
                         </ContactR>
                         </TextWrapper>
                     </TextWrapper>
-                            <Form  noValidate autoComplete="off">
-                                <StTextField required id="standard-basic" label="First Name" />
-                                <StTextField id="standard-basic" label="Last Name" />
-                                <StTextField  required id="standard-basic" label="Email" />
-                                <StTextField id="standard-basic" label="Phone" />
-                                <StTextField id="standard-basic" label="Website" />
-                                <StTextField id="standard-basic" label="Company" />
-                                <StTextareaAutosize required aria-label="minimum height" rowsMin={4} required id="outlined-basic" placeholder="Message" variant="outlined"/>
+                            <Form  noValidate autoComplete="on" >
+                                <StTextField type="text" value={formData.FirstName} name="FirstName" onChange={handleChange} required id="standard-basic" label="First Name" />
+                                <StTextField type="text"  value={formData.LastName} name="LastName" id="standard-basic" label="Last Name" onChange={handleChange}/>
+                                <StTextField type="email" value={formData.Email} name="Email"id="standard-basic" label="Email" required onChange={handleChange}/>
+                                <StTextField type="number" value={formData.Phone}name="Phone"  id="standard-basic" label="Phone" onChange={handleChange} />
+                                <StTextField type="text" value={formData.Website} name="Website" id="standard-basic" label="Website" onChange={handleChange}/>
+                                <StTextField type="text"  value={formData.Company} name="Company"id="standard-basic" label="Company" onChange={handleChange} />
+                                <StTextareaAutosize type="text" value={formData.Message} name="Message" aria-label="minimum height" rowsMin={4} id="outlined-basic" placeholder="Message" variant="outlined" required onChange={handleChange} />
                                 <ButtonWrapper>
-                                     <Button>Send</Button>
-                                     <Button>Clear</Button>
+                                     <Button onClick={handleSubmit} value="Send" >Send</Button>
+                                     <Button onClick={handleClear}>Clear</Button>
                                 </ButtonWrapper>
                             </Form>
                             <TextWrapper>
