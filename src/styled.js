168d6fdb738f8efbdc45c33e8397d7a003738293
gitcrackerUK <pfsiwek@gmail.com>
2021-02-19 10:26:31 +0000

    NI

--- src/styled.js
+++ src/styled.js
@@ -10,7 +10,6 @@ export const boxShadow8 = css`box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.14), 0px 
 export const boxShadow12 = css`box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);`
 
 
-
 export const ImgCover = css`
 background-size: cover;
 background-position: top center; 
