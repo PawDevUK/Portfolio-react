7d57550dafe1fab5357c443bd4e1658b9ab4f213
gitcrackerUK <pfsiwek@gmail.com>
2021-02-22 18:36:04 +0000

    Typos

--- src/store.js
+++ src/store.js
@@ -220,6 +220,6 @@ export const projects = [
 
 export const ReactComponentData = {
     Header: "why react ?",
-    MainText: "I use React because it's great tool allowing to mix HTML/CSS and JavaScript with help of JSX.  Great benefits of React are also quick rendering of content and changing data without reloading page what helps improve user experience.As React uses reusable components, source code is ease to maintain. Data flows in one way so code is stable. React is safe, JSX blocks injection and XSS.",
-    Pros: ["Fast Rendering", "JSX", "Stable Code", "Safety", "Maintenance", "One-way data flow", "Facebook Support", "Reusable Components"]
+    MainText: "I use React because it's great tool allowing to mix HTML and JavaScript with help of JSX.  Great benefits of React are also quick rendering of content and changing data without reloading page what helps improve user experience. As React uses reusable components, source code is ease to maintain. Data flows in one way so code is stable. React is safe, JSX blocks injection and XSS.",
+    Pros: ["Fast Rendering", "JSX", "Stable Code", "Safety", "Maintenance", "One-way data flow", "Facebook Support", "Reusability"]
 }
