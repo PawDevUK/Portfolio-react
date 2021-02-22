9b9bac51dc12b1b9126e0b6c77c37c860ac967b0
gitcrackerUK <pfsiwek@gmail.com>
2021-02-22 18:37:06 +0000

    prettier & added line hight

--- src/components/E-React/pros.jsx
+++ src/components/E-React/pros.jsx
@@ -5,25 +5,24 @@ import { SvgLoader } from 'react-svgmt'
 import React from 'react'
 
 const Wrapper = styled.div`
-margin:0px;
-width:190px;
-display:flex;
-align-items:center;
+  margin: 0px;
+  width: 190px;
+  display: flex;
+  align-items: center;
 `
 const Icon = styled(SvgLoader)`
-margin:0px 4px 0px 0px;
-height:14px;
+  margin: 0px 4px 0px 0px;
+  height: 20px;
 `
 const StP = styled(P)`
-margin:0px;
-padding:0px;
+  margin: 0px;
+  padding: 0px;
 `
 export default function Pros({ ...props }) {
-    return (
-        <Wrapper>
-            <Icon path={icon}></Icon>
-            <StP {...props}>{props.children}</StP>
-        </Wrapper>
-    )
-
+  return (
+    <Wrapper>
+      <Icon path={icon}></Icon>
+      <StP {...props}>{props.children}</StP>
+    </Wrapper>
+  )
 }
