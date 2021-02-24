405c799c5730fb3fd7cda453cb1ff9b274f498a7
gitcrackerUK <pfsiwek@gmail.com>
2021-02-24 23:48:42 +0000

    Typo!!

--- src/components/G-Contact/axiosCreate.js
+++ src/components/G-Contact/axiosCreate.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const SendFormData = axios.create({
-  baseURL: 'https://strong-charge-305714.ew.r.appspot.com/conatctForm'
+  baseURL: 'https://strong-charge-305714.ew.r.appspot.com/contactForm'
 })
 
 export const SendLocalFormData = axios.create({
