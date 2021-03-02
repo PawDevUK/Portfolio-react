0f314642bc88a8d4eb5968e1c8c8810838d025b3
gitcrackerUK <pfsiwek@gmail.com>
2021-03-02 22:16:47 +0000

    added GlobalStyles to TicTacToe.jsx

--- src/components/F-Projects/TicTacToe/TicTacToe.jsx
+++ src/components/F-Projects/TicTacToe/TicTacToe.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { GlobalStyle, flexCenter } from './styled'
 import styled from 'styled-components'
-import { GetRowElement } from 'components/F-Projects/TicTacToe/Row'
-const Wrapper = styled.div``
+import { GetRowElement } from 'components/F-Projects/TicTacToe/components/Row'
 
+const Wrapper = styled.div`
+  background: #cfc5a5;
+  border-radius: 5px;
+  /* width: 100px; */
+`
 const Board = [
   [[1], [2], [3]],
   [[4], [5], [6]],
@@ -12,6 +17,7 @@ const Board = [
 export default function TicTacToe() {
   return (
     <Wrapper>
+      <GlobalStyle></GlobalStyle>
       {GetRowElement(Board, 0)}
       {GetRowElement(Board, 1)}
       {GetRowElement(Board, 2)}
