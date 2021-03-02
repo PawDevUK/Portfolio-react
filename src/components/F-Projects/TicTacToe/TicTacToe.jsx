4ee0b5208d089073feaf6a871084ba72cd709b67
gitcrackerUK <pfsiwek@gmail.com>
2021-03-02 21:43:47 +0000

    Created board arrays and loops to get content

--- src/components/F-Projects/TicTacToe/TicTacToe.jsx
+++ src/components/F-Projects/TicTacToe/TicTacToe.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import { GetRowElement } from 'components/F-Projects/TicTacToe/Row'
 const Wrapper = styled.div``
+
+const Board = [
+  [[1], [2], [3]],
+  [[4], [5], [6]],
+  [[7], [8], [9]],
+]
 
 export default function TicTacToe() {
   return (
     <Wrapper>
-      <h1>Tic Tac Toe</h1>
+      {GetRowElement(Board, 0)}
+      {GetRowElement(Board, 1)}
+      {GetRowElement(Board, 2)}
     </Wrapper>
   )
 }
