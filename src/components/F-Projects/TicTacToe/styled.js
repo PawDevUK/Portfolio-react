222d1fac08d602d58e115e2437a6c6a69797acce
gitcrackerUK <pfsiwek@gmail.com>
2021-03-02 23:00:10 +0000

    boxShadow1/12

--- src/components/F-Projects/TicTacToe/styled.js
+++ src/components/F-Projects/TicTacToe/styled.js
@@ -16,3 +16,9 @@ ${flexCenter}
 }
 `
 
+export const boxShadow1 = css`box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.12), 0px 1px 3px rgba(0, 0, 0, 0.2);`
+export const boxShadow3 = css`box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.14), 0px 3px 3px rgba(0, 0, 0, 0.12), 0px 1px 8px rgba(0, 0, 0, 0.2);`
+export const boxShadow6 = css`box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.14), 0px 1px 18px rgba(0, 0, 0, 0.12), 0px 3px 5px rgba(0, 0, 0, 0.2);
+border-radius:3px;`
+export const boxShadow8 = css`box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.14), 0px 3px 14px rgba(0, 0, 0, 0.12), 0px 5px 5px rgba(0, 0, 0, 0.2);`
+export const boxShadow12 = css`box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);`
