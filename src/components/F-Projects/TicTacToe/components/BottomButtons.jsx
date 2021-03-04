48451c18f4930b536777f4459b56a8c2dce26dfe
gitcrackerUK <pfsiwek@gmail.com>
2021-03-04 15:47:48 +0000

    renamed

--- src/components/F-Projects/TicTacToe/components/BottomButtons.jsx
+++ src/components/F-Projects/TicTacToe/components/BottomButtons.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { StButton } from '../components/common/Button'
+import { StButton } from './common/Button'
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
