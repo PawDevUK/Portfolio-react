b8bcb55658f4537b523e47763751000603379104
gitcrackerUK <pfsiwek@gmail.com>
2021-03-08 11:03:23 +0000

    Add axios get for list of users

--- src/components/F-Projects/TicTacToe/API/routes.js
+++ src/components/F-Projects/TicTacToe/API/routes.js
@@ -2,6 +2,17 @@ import { Server_URL } from '../store'
 import axios from 'axios'
 
 
-export const getUsers = axios.get(`${Server_URL}/users`).then((res) => {
-    return res.data
-})
+export const getUsers = () => {
+    const onlineUsers = axios.get(`${Server_URL}/users`).then((res) => {
+        return res.data
+    })
+    return onlineUsers
+}
+
+export function setUserOnEntry(user) {
+    axios.post(`${Server_URL}/users/addUser`, user).then(res => {
+        console.log(res)
+    }).catch(err => {
+        console.log(err)
+    })
+}
