69e159542865dc8ef13ae17720310462be1b6ff8
gitcrackerUK <pfsiwek@gmail.com>
2021-03-08 12:02:17 +0000

    changed for production url

--- src/components/F-Projects/TicTacToe/store.js
+++ src/components/F-Projects/TicTacToe/store.js
@@ -1,4 +1,5 @@
-export const Server_URL = "http://localhost:8080/tictactoe"
+// export const Server_URL = "http://localhost:8080/tictactoe"
+export const Server_URL = "https://strong-charge-305714.ew.r.appspot.com/tictactoe"
 
 export function InitialBoard() {
     return [
