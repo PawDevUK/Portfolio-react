7082b84ca6cd7cb1f01e60a315015bba3ba049f1
gitcrackerUK <pfsiwek@gmail.com>
2021-03-09 14:03:05 +0000

    localhost

--- src/components/F-Projects/TicTacToe/store.js
+++ src/components/F-Projects/TicTacToe/store.js
@@ -1,6 +1,6 @@
 // export const Server_URL = "http://localhost:8080/tictactoe"
 export const Server_URL = "https://strong-charge-305714.ew.r.appspot.com/tictactoe"
-
+export const localhost = "http://localhost:8080/tictactoe"
 export function InitialBoard() {
     return [
         [[], [], []],
