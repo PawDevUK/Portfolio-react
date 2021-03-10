8af970173cce2363c9be1bd3711f49a00b7f21be
gitcrackerUK <pfsiwek@gmail.com>
2021-03-10 20:03:51 +0000

    Prettier

--- src/components/F-Projects/covid-app/Covid.jsx
+++ src/components/F-Projects/covid-app/Covid.jsx
@@ -1,24 +1,25 @@
-import styled from 'styled-components'
-import { Header, Main, Footer } from './components'
-import { Background } from 'components/F-Projects/covid-app/components/styled'
-import React from 'react'
+import { Background } from 'components/F-Projects/covid-app/components/styled';
+import { Header, Main, Footer } from './components';
+import styled from 'styled-components';
+import React from 'react';
 
 export const Container = styled.div`
-  font-family: 'Lato', sans-serif;
-  ${Background};
-  width: 1000px;
-  margin: 0px auto;
-  padding: 1px;
-  @media (max-width: 1000px) {
-    width: 100%;
-  }
-`
+    font-family: 'Lato', sans-serif;
+    ${Background};
+    width: 1000px;
+    margin: 0px auto;
+    padding: 1px;
+    @media (max-width: 1000px) {
+        width: 100%;
+    }
+`;
+
 export default function App() {
-  return (
-    <Container>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
-    </Container>
-  )
+    return (
+        <Container>
+            <Header></Header>
+            <Main></Main>
+            <Footer></Footer>
+        </Container>
+    );
 }
