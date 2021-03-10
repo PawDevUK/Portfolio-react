8dabd4939459f5f85907271640e4f439bbd3fb4e
gitcrackerUK <pfsiwek@gmail.com>
2021-03-10 09:49:37 +0000

    Added comp Board

---
+++ src/components/F-Projects/TicTacToe/components/Board/Board.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+import Row from '../Row';
+import TopButtons from './TopButtons';
+import BottomButtons from './BottomButtons';
+import CrossLine from './CrossLine';
+
+export default function Board() {
+    return <div></div>;
+}
