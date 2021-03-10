276104a97b2db61d3bdec365d7b6fa4f557bf0d8
gitcrackerUK <pfsiwek@gmail.com>
2021-03-10 19:11:39 +0000

    Prettier

--- src/components/F-Projects/TicTacToe/components/Header.jsx
+++ src/components/F-Projects/TicTacToe/components/Header.jsx
@@ -1,85 +1,84 @@
-import React, { useState, useEffect } from 'react'
-import styled, { css } from 'styled-components'
-import TextField from '@material-ui/core/TextField'
-import { boxShadow3 } from '../styled'
-import { setUserOnEntry } from '../API/routes'
+import React, { useState, useEffect } from 'react';
+import styled, { css } from 'styled-components';
+import TextField from '@material-ui/core/TextField';
+import { boxShadow3 } from '../styled';
+import { setUserOnEntry } from '../API/routes';
 
 const Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 96px;
-`
+    position: relative;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 96px;
+`;
 const InputWrapper = styled.form`
-  position: absolute;
-  left: 230px;
-  ${(p) =>
-    p.nameSubmit
-      ? css`
-          opacity: 0;
-          transition: all 0.5s ease-in;
-        `
-      : css``}
-`
+    position: absolute;
+    left: 230px;
+    ${(p) =>
+        p.nameSubmit
+            ? css`
+                  opacity: 0;
+                  transition: all 0.5s ease-in;
+              `
+            : css``}
+`;
 const UsernameWrapper = styled.div`
-  margin: auto;
+    margin: auto;
 
-  ${(p) =>
-    !p.nameSubmit
-      ? css`
-          opacity: 0;
-          transition: all 1s ease-in;
-        `
-      : css`
-          opacity: 1;
-          transition: all 1s ease-in;
-        `}
-`
+    ${(p) =>
+        !p.nameSubmit
+            ? css`
+                  opacity: 0;
+                  transition: all 1s ease-in;
+              `
+            : css`
+                  opacity: 1;
+                  transition: all 1s ease-in;
+              `}
+`;
 const H1 = styled.h1`
-  color: #fff;
-  font-weight: bold;
-  background-color: #cfc5a5;
-  border-radius: 5px;
-  padding: 10px;
-  ${boxShadow3};
-`
+    color: #fff;
+    font-weight: bold;
+    background-color: #cfc5a5;
+    border-radius: 5px;
+    padding: 10px;
+    ${boxShadow3};
+`;
 
 export default function Header(props) {
-  const [name, setName] = useState('')
-  const [nameSubmit, setSubmit] = useState(false)
-  useEffect(() => {}, [])
-  const capitalize = (s) => {
-    if (typeof s !== 'string') return ''
-    return s.charAt(0).toUpperCase() + s.slice(1)
-  }
-  return (
-    <Wrapper>
-      <InputWrapper
-        nameSubmit={nameSubmit}
-        onSubmit={(e) => {
-          props.Submit(e, name)
-          setUserOnEntry({
-            user: name,
-            status: 'Available',
-            playingWith: '---',
-            board: [],
-          })
-          setSubmit(e.type ? true : false)
-        }}
-      >
-        <TextField
-          onChange={(e) => {
-            setName(e.target.value)
-          }}
-          id="user"
-          label="Username"
-          variant="outlined"
-        />
-      </InputWrapper>
-      <UsernameWrapper nameSubmit={nameSubmit}>
-        <H1>{capitalize(name)}</H1>
-      </UsernameWrapper>
-    </Wrapper>
-  )
+    const [name, setName] = useState('');
+    const [nameSubmit, setSubmit] = useState(false);
+    useEffect(() => {}, []);
+    const capitalize = (s) => {
+        if (typeof s !== 'string') return '';
+        return s.charAt(0).toUpperCase() + s.slice(1);
+    };
+    return (
+        <Wrapper>
+            <InputWrapper
+                nameSubmit={nameSubmit}
+                onSubmit={(e) => {
+                    props.Submit(e, name);
+                    setUserOnEntry({
+                        user: name,
+                        status: 'Available',
+                        playingWith: '---',
+                        board: [],
+                    });
+                    setSubmit(e.type ? true : false);
+                }}>
+                <TextField
+                    onChange={(e) => {
+                        setName(e.target.value);
+                    }}
+                    id='user'
+                    label='Username'
+                    variant='outlined'
+                />
+            </InputWrapper>
+            <UsernameWrapper nameSubmit={nameSubmit}>
+                <H1>{capitalize(name)}</H1>
+            </UsernameWrapper>
+        </Wrapper>
+    );
 }
