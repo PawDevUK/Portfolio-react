052d140b7e37aba461e132c3bb9a8ede639cb9c1
gitcrackerUK <pfsiwek@gmail.com>
2021-03-10 20:36:28 +0000

    Prettier

--- src/components/F-Projects/TicTacToe/API/routes.js
+++ src/components/F-Projects/TicTacToe/API/routes.js
@@ -1,18 +1,20 @@
-import { Server_URL } from '../store'
-import axios from 'axios'
+import { Server_URL } from '../store';
+import axios from 'axios';
 
-
-export const getUsers = () => {
+export function getUsers() {
     const onlineUsers = axios.get(`${Server_URL}/users`).then((res) => {
-        return res.data
-    })
-    return onlineUsers
+        return res.data;
+    });
+    return onlineUsers;
 }
 
 export function setUserOnEntry(user) {
-    axios.post(`${Server_URL}/users/addUser`, user).then(res => {
-        console.log(res)
-    }).catch(err => {
-        console.log(err)
-    })
+    axios
+        .post(`${Server_URL}/users/addUser`, user)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
--- src/components/F-Projects/TicTacToe/store.js
+++ src/components/F-Projects/TicTacToe/store.js
@@ -1,8 +1,11 @@
 // export const Server_URL = "http://localhost:8080/tictactoe"
 import { topRbottL, topLbottR, topLR, midLR, bottomLR, leftTopBottom, midTopBottom, rightTopBottom } from './img';
 export const Scenario = [topRbottL, topLbottR, topLR, midLR, bottomLR, leftTopBottom, midTopBottom, rightTopBottom];
+
 export const Server_URL = 'https://strong-charge-305714.ew.r.appspot.com/tictactoe';
+
 export const localhost = 'http://localhost:8080/tictactoe';
+
 export function InitialBoard() {
     return [
         [[], [], []],
