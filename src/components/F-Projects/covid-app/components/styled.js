022db4812d6fb054897d5fc5b2ce29d26e170bb8
gitcrackerUK <pfsiwek@gmail.com>
2021-03-14 20:55:38 +0000

    Prettier + flexcenter

--- src/components/F-Projects/covid-app/components/styled.js
+++ src/components/F-Projects/covid-app/components/styled.js
@@ -1,51 +1,77 @@
-import {css } from 'styled-components';
+import { css } from 'styled-components';
 
 //colors------------
-export const Background = css`background-color:#f1f3f4;`
-export const CountryData = css`background-color:#F1F3F4;`
-export const GlobalBackground = css`background-color:#CCF2F4;`
-export const GlobalMain = css`background-color:#79BAC1; opacity:0.9;`
-export const SectionHeaders = css`background-color:#C7CED9;`
-export const FootBackGround = css`background-color:#C7CED9;`
+export const Background = css`
+    background-color: #f1f3f4;
+`;
+export const CountryData = css`
+    background-color: #f1f3f4;
+`;
+export const GlobalBackground = css`
+    background-color: #ccf2f4;
+`;
+export const GlobalMain = css`
+    background-color: #79bac1;
+    opacity: 0.9;
+`;
+export const SectionHeaders = css`
+    background-color: #c7ced9;
+`;
+export const FootBackGround = css`
+    background-color: #c7ced9;
+`;
 //------------------
 
 //fonts------------------------------------------------------------------------------------------------------------------------------
-// weight -extra-light 200  
+// weight -extra-light 200
 //        -light 300
 //        -regular 400
 //        -medium 500
 export const h1Light = css`
-font-size:20px;
-font-weight:300;
-`
+    font-size: 20px;
+    font-weight: 300;
+`;
 export const h1LightRES = css`
-font-size:2vw;
-font-weight:300;
-`
+    font-size: 2vw;
+    font-weight: 300;
+`;
 export const h1Regular = css`
-color:black;
-font-size:20px;
-font-weight:400;
-`
+    color: black;
+    font-size: 20px;
+    font-weight: 400;
+`;
 export const h2Light = css`
-font-size:18px;
-font-weight:300;
-`
+    font-size: 18px;
+    font-weight: 300;
+`;
 export const h2Regular = css`
-font-size:18px;
-font-weight:400;
-`
+    font-size: 18px;
+    font-weight: 400;
+`;
 export const h3Light = css`
-font-size:16px;
-font-weight:300;
-`
+    font-size: 16px;
+    font-weight: 300;
+`;
 export const h3Regular = css`
-font-size:16px;
-font-weight:400;
-`
+    font-size: 16px;
+    font-weight: 400;
+`;
 //--------------------------------------------------------------------------------------------------------------------------------------
 
-export const shadow6 = css`box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.14), 0px 1px 18px rgba(0, 0, 0, 0.12), 0px 3px 5px rgba(0, 0, 0, 0.2);
-border-radius:3px;`
-export const shadow8 = css`box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.14), 0px 3px 14px rgba(0, 0, 0, 0.12), 0px 5px 5px rgba(0, 0, 0, 0.2);`
-export const shadow12 = css`box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);`
+export const shadow6 = css`
+    box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.14), 0px 1px 18px rgba(0, 0, 0, 0.12), 0px 3px 5px rgba(0, 0, 0, 0.2);
+    border-radius: 3px;
+`;
+export const shadow8 = css`
+    box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.14), 0px 3px 14px rgba(0, 0, 0, 0.12), 0px 5px 5px rgba(0, 0, 0, 0.2);
+`;
+export const shadow12 = css`
+    box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);
+`;
+
+//-------------------------------------
+export const FlexCenter = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
