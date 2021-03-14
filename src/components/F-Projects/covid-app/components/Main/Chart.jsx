9d5b1b1110775cf803d79e657a383254d5d82d2e
gitcrackerUK <pfsiwek@gmail.com>
2021-03-14 21:40:39 +0000

    prettier reduced margin for chart

--- src/components/F-Projects/covid-app/components/Main/Chart.jsx
+++ src/components/F-Projects/covid-app/components/Main/Chart.jsx
@@ -1,32 +1,31 @@
-import { ResponsiveBar } from '@nivo/bar'
-import {CheckMax} from '../factory';
+import { ResponsiveBar } from '@nivo/bar';
+import { CheckMax } from '../factory';
 import React from 'react';
 
-export function Chart({ data }){
-
-    return(
+export function Chart({ data }) {
+    return (
         <ResponsiveBar
-        enableGridX={true}
-        data={data}
-        keys={[ 'active cases', 'new cases', 'new deaths' ]}
-        indexBy="id"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-        padding={0}
-        groupMode="grouped"
-        layout="horizontal"
-        colors={{ scheme: 'accent' }}
-        borderRadius={4}
-        borderColor={{ from: 'color', modifiers: [ [ 'darker', '1.3' ] ] }}
-        // axisTop={{ tickSize: 10, tickPadding: 8, tickRotation: 45, legend: '', legendOffset: 27 }}
-         maxValue={CheckMax(data[0])}
-        axisRight={null}
-        labelSkipWidth={17}
-        labelSkipHeight={12}
-        innerPadding={6}
-        labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.8 ] ] }}
-        legends={[
-            {
-                dataFrom: 'keys',
+            enableGridX={true}
+            data={data}
+            keys={['active cases', 'new cases', 'new deaths']}
+            indexBy='id'
+            margin={{ top: 20, right: 130, bottom: 10, left: 60 }}
+            padding={0}
+            groupMode='grouped'
+            layout='horizontal'
+            colors={{ scheme: 'accent' }}
+            borderRadius={4}
+            borderColor={{ from: 'color', modifiers: [['darker', '1.3']] }}
+            // axisTop={{ tickSize: 10, tickPadding: 8, tickRotation: 45, legend: '', legendOffset: 27 }}
+            maxValue={CheckMax(data[0])}
+            axisRight={null}
+            labelSkipWidth={17}
+            labelSkipHeight={12}
+            innerPadding={6}
+            labelTextColor={{ from: 'color', modifiers: [['darker', 1.8]] }}
+            legends={[
+                {
+                    dataFrom: 'keys',
                     anchor: 'right',
                     direction: 'column',
                     justify: false,
@@ -37,22 +36,19 @@ export function Chart({ data }){
                     itemsSpacing: 12,
                     symbolSize: 14,
                     itemDirection: 'right-to-left',
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
-        motionStiffness={100}
-        motionDamping={10}
-        animate={true}
-    />)
-
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemOpacity: 1,
+                            },
+                        },
+                    ],
+                },
+            ]}
+            motionStiffness={100}
+            motionDamping={10}
+            animate={true}
+        />
+    );
 }
-
-    
-   
--- src/components/F-Projects/covid-app/components/common/SectionHeader.jsx
+++ src/components/F-Projects/covid-app/components/common/SectionHeader.jsx
@@ -1,51 +1,44 @@
-import {
-  SectionHeaders,
-  h1Light,
-  h1LightRES,
-  h3Light,
-  shadow6,
-} from 'components/F-Projects/covid-app/components/styled'
-import styled, { css } from 'styled-components'
-import React from 'react'
+import { SectionHeaders, h1Light, h1LightRES, h3Light, shadow6 } from 'components/F-Projects/covid-app/components/styled';
+import styled, { css } from 'styled-components';
+import React from 'react';
 
 const Wrapper = styled.div`
-  justify-content: center;
-  align-items: center;
-  border-radius: 3px;
-  ${SectionHeaders};
-  display: flex;
-  ${(p) =>
-    p.Large
-      ? css`
-          position: absolute;
-          width: 269px;
-          height: 40px;
-          ${h1Light}
-          ${shadow6};
-          top: 40px;
-          left: 40px;
-          @media (max-width: 1000px) {
-            top: 4vw;
-            left: 3vw;
-            width: 27vw;
-            height: 4.5vw;
-            ${h1LightRES}
-          }
-        `
-      : css``}
+    justify-content: center;
+    align-items: center;
+    border-radius: 3px;
+    ${SectionHeaders};
+    display: flex;
+    ${(p) =>
+        p.Large
+            ? css`
+                  position: absolute;
+                  width: 269px;
+                  height: 40px;
+                  ${h1Light}
+                  ${shadow6};
+                  top: 40px;
+                  left: 40px;
+                  @media (max-width: 1000px) {
+                      top: 4vw;
+                      left: 3vw;
+                      width: 27vw;
+                      height: 4.5vw;
+                      ${h1LightRES}
+                  }
+              `
+            : css``}
 
-  ${(p) =>
-    p.Regular
-      ? css`
-          margin: 5px auto 0px auto;
-          /* padding-left: 60px; */
-          ${shadow6};
-          ${h3Light};
-          width: 80%;
-        `
-      : css``}
-`
+    ${(p) =>
+        p.Regular
+            ? css`
+                  margin: 5px auto 0px auto;
+                  ${shadow6};
+                  ${h3Light};
+                  width: 80%;
+              `
+            : css``}
+`;
 
 export default function SectionHeader(props) {
-  return <Wrapper {...props}></Wrapper>
+    return <Wrapper {...props}></Wrapper>;
 }
