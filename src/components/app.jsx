03ad5dd320e1eafd0165f68d029d6340ce0133ca
GitcrackerUK <pfsiwek@gmail.com>
2022-05-04 22:31:26 +0100

    imported useEffect and used to sync DB and Config

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,22 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { store } from '../configureStore'
 import { Provider } from 'react-redux'
-import sync_DB_With_Store from '../configureStore'
+import {sync_DB_With_Store, syncConfigWithStore } from '../configureStore'
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 import Router from '../routes/routes'
 
-sync_DB_With_Store()
+
 
 
 function App() {
+
+  useEffect(()=>{
+    sync_DB_With_Store();
+    syncConfigWithStore();
+  })
+    
   return (
     <Provider store={store}>
       <Router></Router>
