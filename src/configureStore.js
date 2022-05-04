ba6095382bfd4d5f86d0bf427680c0df95688ffc
GitcrackerUK <pfsiwek@gmail.com>
2022-05-04 18:43:05 +0100

    added action addCounter and dispatch function

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,6 +1,8 @@
 import {
     configureStore
 } from '@reduxjs/toolkit'
+import {LocalOnEnter} from './config/VisitorsAPI.config'
+import addCounter from 'actions/addCounter'
 
 import rootReducer from './reducers'
 
@@ -9,3 +11,10 @@ const middleware = []
 export const store = configureStore({
     reducer:rootReducer()
 })
+
+export default function sync_DB_With_Store(){
+    LocalOnEnter.get('/').then((res) => {
+     store.dispatch(addCounter( res.data[res.data.length-1].counter ))
+    });
+}
+
