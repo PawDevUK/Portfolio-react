3e900acf405052055f0e055e9322e17a2aea8b73
GitcrackerUK <pfsiwek@gmail.com>
2022-05-04 20:38:45 +0100

    removed non existing URL

--- src/config/VisitorsAPI.config.js
+++ src/config/VisitorsAPI.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const onEnter = axios.create({
-    baseURL: 'https://strong-charge-305714.ew.r.appspot.com/visitor/recordVisit',
+    baseURL: '',
 })
 export const LocalOnEnter = axios.create({
     baseURL: 'http://localhost:8080/visitor',
