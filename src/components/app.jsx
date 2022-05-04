7b8573ef2091c84d91469c5835ec149a52eca6f9
GitcrackerUK <pfsiwek@gmail.com>
2022-05-04 18:44:02 +0100

    added funct to sync DB with store

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,9 +2,13 @@
 import React from 'react'
 import { store } from '../configureStore'
 import { Provider } from 'react-redux'
+import sync_DB_With_Store from '../configureStore'
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 import Router from '../routes/routes'
+
+sync_DB_With_Store()
+
 
 function App() {
   return (
