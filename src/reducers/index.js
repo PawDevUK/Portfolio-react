a93d5026fc6e012656ba9274d4564f93991c379d
GitcrackerUK <pfsiwek@gmail.com>
2022-05-04 22:36:46 +0100

    added FooterData to combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import Visitors from 'reducers/counterReducer'
+import FooterData from 'reducers/footerData.reducer'
 import Users from 'reducers/users'
 
 
 export default function rootReducer(){
     return combineReducers({
         Visitors,
-        Users
+        Users,
+        FooterData,
     })
 }
