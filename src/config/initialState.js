743ba67170bec420b4af7dc2a20bc7c615a8c72e
GitcrackerUK <pfsiwek@gmail.com>
2022-05-07 13:09:50 +0100

    created initialState object for PageContent redu..

---
+++ src/config/initialState.js
@@ -0,0 +1,11 @@
+export let initialState = {
+        header:{
+            menu:[]
+        },
+        projects:[],
+        reactInfo:{},
+        stack:[],
+        footer:{
+            rightData:[]
+        },
+    }
