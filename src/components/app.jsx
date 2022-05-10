e86b29c70417fbed52d912a7bd89926b25ec16f7
GitcrackerUK <pfsiwek@gmail.com>
2022-05-10 22:12:32 +0100

    added sass

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 import { store } from '../configureStore'
 import { Provider } from 'react-redux'
 import {sync_DB_With_Store, syncConfigWithStore } from '../configureStore'
-
+import '../styles/themes.css'
 import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 import Router from '../routes/routes'
 
