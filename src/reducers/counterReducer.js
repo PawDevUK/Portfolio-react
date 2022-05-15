6f3eefa16039f8b18df860857f6fd2c5b03f2104
GitcrackerUK <pfsiwek@gmail.com>
2022-05-16 00:17:37 +0100

    code refactor

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -2,10 +2,9 @@
 export default function Visitors(state = 0 , action){
     switch(action.type){
         case 'ADD_COUNTER':
-            return {
-                ...state,
-                counter: action.payload
-            };
+            
+            return action.payload
+          
             default:
                 return state
     }
