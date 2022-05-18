4761a40bee90b22864c83d8938385a3c56fd1e59
GitcrackerUK <p.f.siwek@gmail.com>
2022-05-18 20:24:40 +0100

    added lang support to menu obj

--- src/config/headerMenu.config.js
+++ src/config/headerMenu.config.js
@@ -3,29 +3,44 @@ export const Menu = [
         title: "About",
         href: "#about",
         disabled: false,
+        PolishLang:{
+            title:"Trochę o mnie"
+        }
     },
     {
         title: "What I Use",
         href: "#use",
         disabled: false,
+        PolishLang:{
+            title:"Co używam..."
+        },
         react: {
             title: "Why I use React ?",
             href: "#react",
             style: "Blue",
-            disabled: false
+            disabled: false,
+            PolishLang:{
+                title:"Dlaczego używam React"
+            }
         }
     },
     {
         title: "Projects",
         href: "#projects",
         style: "Green",
-        disabled: false
+        disabled: false,
+        PolishLang:{
+            title:"Projekty"
+        }
     },
     {
         title: "My Github",
         href: "https://github.com/GitcrackerUK",
         disabled: false,
-        target: "_blank"
+        target: "_blank",
+        PolishLang:{
+            title:"Mój Github"
+        }
     },
     {
         title: "CV",
@@ -37,5 +52,8 @@ export const Menu = [
         title: "Contact",
         href: "#contact",
         disabled: true,
+        PolishLang:{
+            title:"Kontakt"
+        }
     }
 ]
