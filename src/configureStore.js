e2e67a37bd704d7bae32bf455f70d40ad61c7206
GitcrackerUK <pfsiwek@gmail.com>
2022-06-13 16:03:42 +0100

    added new state to redux headerJumboText & buttons

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,7 +3,7 @@ import {
 } from '@reduxjs/toolkit'
 import {LocalOnEnter} from './config/VisitorsAPI.config'
 import addCounter from 'actions/addCounter.actions'
-import { AddHeaderMenu, AddProjects, AddReactInfo, AddStack, AddFooterRightData } from 'actions/pageContent.actions'
+import { AddHeaderMenu, AddHeaderJumboText, AddProjects, AddReactInfo, AddStack, AddFooterRightData, AddButtons } from 'actions/pageContent.actions'
 import rootReducer from './reducers'
 import { rightData } from 'config/footer.config'
 import { Menu } from 'config/headerMenu.config'
@@ -12,6 +12,8 @@ import { projects } from 'config/projects.config'
 import { ReactComponentData } from 'config/reactComponent.config'
 import logger from 'redux-logger'
 import { addUUID_ToObject } from 'factory'
+import { headerJumboText } from 'config/headerJumboText.config'
+import { buttons } from 'config/common.config'
 
 const middleware = [logger]
 
@@ -32,4 +34,6 @@ export function syncConfigWithStore(){
     store.dispatch(AddReactInfo(addUUID_ToObject(ReactComponentData)))
     store.dispatch(AddProjects(addUUID_ToObject(projects)))
     store.dispatch(AddFooterRightData(rightData))
+    store.dispatch(AddHeaderJumboText(addUUID_ToObject(headerJumboText)))
+    store.dispatch(AddButtons(addUUID_ToObject(buttons)))
 }
