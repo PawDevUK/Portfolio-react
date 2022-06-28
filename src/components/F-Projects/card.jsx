f79b082a8d1147b234ef3e8aaf9f4eb2c7e16f09
GitcrackerUK <pfsiwek@gmail.com>
2022-06-28 22:11:00 +0100

    removed spacing

--- src/components/F-Projects/card.jsx
+++ src/components/F-Projects/card.jsx
@@ -6,43 +6,46 @@ import React from 'react'
 import Stack from './Stack'
 
 const Header = styled.h5`
-  font-size: 18px;
-  transition: transform 0.5s;
+  font-size:18px;
+  transition:transform 0.5s;
 `
 const Wrapper = styled.div`
   z-index:1000;
   background-color:#fff;
+  border:solid 1px #dbdbdb;
+  margin:20px auto 0px auto;
   padding:4px;
+  border-radius:10px;
   ${boxShadow12};
-  width: 60%;
-  @media (max-width: 600px) {
-    width: 100%;
+  width:60%;
+  @media (max-width:600px) {
+    width:100%;
   }
-  @media (min-width: 992px) {
-    height: 430px;
-    width: 40%;
+  @media (min-width:992px) {
+    height:430px;
+    width:40%;
   }
   &:hover ${Header} {
     transform: scale(1.2);
   }
 `
 const CardContent = styled.div`
-  z-index: 10000;
-  text-align: center;
-  margin: 0 5px 10px 5px;
+  z-index:10000;
+  text-align:center;
+  margin:0 5px 10px 5px;
 `
 const StImg = styled.img`
-  display: block;
-  margin: auto;
-  height: 200px;
-  width: 80%;
+  display:block;
+  margin:auto;
+  height:200px;
+  width:80%;
 `
 
 const ButtonWrapper = styled.div`
-  justify-content: space-between;
-  margin: 10px auto auto auto;
-  display: flex;
-  width: 250px;
+  justify-content:space-between;
+  margin:10px auto auto auto;
+  display:flex;
+  width:250px;
 `
 const P = styled.p`
 font-size:16px;
@@ -61,7 +64,6 @@ export default function Card({ ...props }) {
       <CardContent>
         <Header className="card-title">{props.item.title}</Header>
         <P className="card-text">{LimitText(props.item.text, 150)}</P>
-
         <ButtonWrapper>
           <A href={props.item.webHref.href} target="_blank">
             <Button light>{props.item.webHref.button}</Button>
