7c87ba17007f1589a29b7419cff7f87f53cf8001
GitcrackerUK <pfsiwek@gmail.com>
2022-07-06 18:54:46 +0100

    removed box shadow from chat icon

--- src/components/Z-Chat/Chat.jsx
+++ src/components/Z-Chat/Chat.jsx
@@ -20,8 +20,6 @@ const Button = styled(SvgLoader)`
       ? css`
           transition: margin-right 0.4s;
           margin-right: 5px;
-          box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.14),
-            0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2);
         `
       : css``}
 
