7d37c42ab0bd31a90ad8ea6b2ab88f586eb4dab6
GitcrackerUK <pfsiwek@gmail.com>
2022-07-07 10:06:33 +0100

    added regex for user and pwd

--- src/components/Register_Login/Register.jsx
+++ src/components/Register_Login/Register.jsx
@@ -2,6 +2,9 @@ import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,24}$/;
+
 export default function Register() {
     return (
         <div>Register</div>
