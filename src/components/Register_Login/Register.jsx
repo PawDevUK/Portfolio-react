ea008ce7dc9934509a3d69da4843793659518639
GitcrackerUK <pfsiwek@gmail.com>
2022-07-07 10:08:04 +0100

    added refs and states for user, pwd

--- src/components/Register_Login/Register.jsx
+++ src/components/Register_Login/Register.jsx
@@ -6,6 +6,24 @@ const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,24}$/;
 
 export default function Register() {
+    const userRef = useRef()
+    const errRef = useRef()
+
+    const [user, setuser] = useState('')
+    const [validate, setValidate] = useState(false)
+    const [userFocus, setuserFocus] = useState(false)
+
+    const [pwd, setPwd] = useState('')
+    const [validPwd, setValidPwd] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false)
+
+    const [matchPwd, setMatchPwd] = useState('')
+    const [validMatchPwd, setValidMatchPwd] = useState(false)
+    const [matchFocusPwd, setMatchFocus] = useState(false)
+
+    const [errMsg, setErrMsg] = useState('')
+    const [successMsg, setSuccessMsg] = useState('')
+
     return (
         <div>Register</div>
     )
