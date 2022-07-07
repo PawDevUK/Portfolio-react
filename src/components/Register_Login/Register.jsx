f31fd8b5c171ecc5c3e14ed8086a2a9717d1837c
GitcrackerUK <pfsiwek@gmail.com>
2022-07-07 10:05:45 +0100

    created Register component

---
+++ src/components/Register_Login/Register.jsx
@@ -0,0 +1,9 @@
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Register() {
+    return (
+        <div>Register</div>
+    )
+}
