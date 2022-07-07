44c8e0645fb5a5706d94b59803e8ca8b18edda7b
GitcrackerUK <pfsiwek@gmail.com>
2022-07-07 10:08:51 +0100

    added useEffect to focus on load on user field

--- src/components/Register_Login/Register.jsx
+++ src/components/Register_Login/Register.jsx
@@ -6,6 +6,7 @@ const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,24}$/;
 
 export default function Register() {
+
     const userRef = useRef()
     const errRef = useRef()
 
@@ -24,6 +25,9 @@ export default function Register() {
     const [errMsg, setErrMsg] = useState('')
     const [successMsg, setSuccessMsg] = useState('')
 
+    useEffect(()=>{
+        userRef.current.focus();
+    },[])
     return (
         <div>Register</div>
     )
