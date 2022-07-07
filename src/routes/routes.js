55f3b94c71d0f0d23a0bf2a6f5b068f1a08ee51a
GitcrackerUK <pfsiwek@gmail.com>
2022-07-07 11:49:56 +0100

    added registered component and route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import styled, { createGlobalStyle } from 'styled-components'
 import TicTacToe from '../components/F-Projects/TicTacToe/TicTacToe'
+import Register from 'components/Register_Login/Register'
 import Projects from '../components/F-Projects/projects'
 import Footer from '../components/H-Footer/Footer.jsx'
 import TopIntro from '../components/C-About/top-intro'
@@ -13,6 +14,7 @@ import Intro from '../components/A-Intro/intro'
 import Chat from '../components/Z-Chat/Chat'
 import CV from '../components/I-CV/cv'
 import React from 'react'
+
 
 export default function Router(){
     
@@ -68,6 +70,9 @@ const Container = styled.div`
             </Route>
             <Route exact path='/cv' >
               <CV></CV>
+            </Route>
+            <Route exact path='/register' >
+              <Register></Register>
             </Route>
             <Route exact path="/">
               <Intro></Intro>
