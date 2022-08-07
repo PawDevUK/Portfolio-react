a7b41ca43dfed02662761122859ae37e5ece1067
GitcrackerUK <pfsiwek@gmail.com>
2022-08-07 22:45:49 +0100

    changed div into p

--- src/config/topIntro.config.js
+++ src/config/topIntro.config.js
@@ -5,33 +5,33 @@ export const intro = {
     text:'<div class=styledP>Jestem developerem z Reading Uk....</div>'
   },
   text:`
-  <div class=styledP>
+  <p class=styledP>
   I am a <span class="bold">React</span> developer from Reading, UK. I create React
   web apps based on components styled in <br />
   <span class="bold">styled-components</span>. I use mainly JavaScript for front-end
   and have some knowledge of Node.Js for back-end and MongoDB.
-</div>
-<div class=styledP>
+</p>
+<p class=styledP>
   I design and create, modern looking websites and web applications with
   functionality including <span class="bold">APIs</span>. I use <span class="bold">Figma</span> to
   to design a modern looking layout with the use of number of popular
   design patterns. Currently, most of the internet users browse Web on
   mobile devices therefore <span class="bold">RWD</span> is quite important that is
   why I am implementing it in my websites and applications.
-</div>
-<div class=styledP>
+</p>
+<p class=styledP>
   I use <span class="bold">React</span> which requires good knowledge of number of
   technologies like <span class="bold">JavaScript ES6</span>, HTML, CSS, Package
   Manager, WebPack, Babel and many more. Most of my work is kept via Git
   CLI on <span class="bold">GitHub</span> so I branch, commit, push, pull and merge
   fairly often.
-</div>
-<div class=styledP>
+</p>
+<p class=styledP>
   All my work is done on <span class="bold">Linux</span> machine so Terminal is used
   on a regular basis. Code editor of my choice is
   <span class="bold">Visual Studio Code</span>. Also I know how to build a backend
   for simple communication(express.js) with frontend(Axios) and
   database.
-</div>
+</p>
 `
 }
