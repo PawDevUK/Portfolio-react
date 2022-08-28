a5aaade79a98a875aacec4d499e770d136e90787
GitcrackerUK <pfsiwek@gmail.com>
2022-08-28 22:28:05 +0100

    passed visitors props to component

--- src/components/H-Footer/VisitorCounter.jsx
+++ src/components/H-Footer/VisitorCounter.jsx
@@ -1,6 +1,5 @@
-import  React, { useEffect, useState } from 'react'
+import  React from 'react'
 import styled from 'styled-components'
-// import { LocalOnEnter } from 'config/VisitorsAPI.config.js'
 
 const Wrapper = styled.div`
   display:block;
@@ -8,19 +7,10 @@ const Wrapper = styled.div`
   text-align:center;
 `
 export default function VisitorCounter(props) {
-
   const H2 = props.h2
-  const [visits, setVisits] = useState(props.visits)
-
-  // useEffect(() => {
-  //   LocalOnEnter.get('/').then((res) => {
-  //     setVisits(res.data[res.data.length-1].counter);
-  //   });
-  // },[]);
-
   return (
     <Wrapper>
-      <H2>Visits {visits}</H2>
+      <H2>Visits {props.visitors}</H2>
     </Wrapper>
   )
 }
