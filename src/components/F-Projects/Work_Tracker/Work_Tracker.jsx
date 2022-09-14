62a7f333a21a58fdc23dad2984d1a1d309194b88
GitcrackerUK <pfsiwek@gmail.com>
2022-09-15 00:31:59 +0100

    imported DayTime and style

--- src/components/F-Projects/Work_Tracker/Work_Tracker.jsx
+++ src/components/F-Projects/Work_Tracker/Work_Tracker.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import s from './style.module.css'
-
+import s from './style/style.module.css'
+import DayTime from './dayTime/DayTime.jsx'
 
 export default class WorkTracker extends Component {
     render() {
         return (
             <div id={s.wrapper}>
                 <h1>Work Tracker</h1>
+                <DayTime></DayTime>
             </div>
         );
     }
