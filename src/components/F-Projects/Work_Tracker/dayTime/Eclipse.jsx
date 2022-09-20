3cc6512b9af2ad05dc4e099d84daa3c33ead089f
GitcrackerUK <pfsiwek@gmail.com>
2022-09-21 00:49:53 +0100

    created status chart with dynamically rendered basic pay and overtime pay

--- src/components/F-Projects/Work_Tracker/dayTime/Eclipse.jsx
+++ src/components/F-Projects/Work_Tracker/dayTime/Eclipse.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
 import s from '../style/style.module.css';
+import React, { useState, useEffect } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core';
 
-export default function Eclipse() {
+export default function Eclipse(props) {
+    const [pay, setPay] = useState(0);
+    const [basic, setBasic] = useState(0);
+    const [overtime, setOvertime] = useState(0);
+
+    useEffect(() => {
+        setBasic(props.basic);
+        setOvertime(props.overTime);
+        setPay(props.basic + props.overTime);
+    }, [props]);
+
+    //styles
+    const SCBlue = withStyles({
+        root: {
+            position: 'absolute',
+            color:'#283daf'
+        },
+    })(CircularProgress);
+
+    const SCGreen = withStyles({
+        root: {
+            position: 'absolute',
+            color: '#28b828',
+        }
+    })(CircularProgress);
+    ///
+
+    function calculatePercent(basic,overtime){
+        return overtime / (( basic + overtime) / 100)
+    }
+
     return (
         <div>
             <div id={s.circleWrapper}>
-                <div id={s.outerCircle}></div>
+                <div className={s.SCWrapper}>
+                    <SCBlue color='primary' size={144} thickness={10} variant='determinate' value='100' />
+                    <SCGreen color='secondary' size={144} thickness={10} variant='determinate' value={calculatePercent(basic,overtime)} />
+                </div>
+
                 <div id={s.innerCircle}>
-                    <div id={s.text}>
-                        £220.20
-                    </div>
+                    <div id={s.text}>{`£${pay}`}</div>
                 </div>
             </div>
         </div>
