9994c9452880212bd25794507ec3379110c7c224
GitcrackerUK <pfsiwek@gmail.com>
2022-09-21 17:38:01 +0100

    added constructor and createShift

--- src/components/F-Projects/Work_Tracker/dayTime/DayTime.jsx
+++ src/components/F-Projects/Work_Tracker/dayTime/DayTime.jsx
@@ -4,6 +4,30 @@ import DayTimeLine from './DayTimeLine';
 import Eclipse from './Eclipse';
 
 export default class DayTime extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // arr: ['20:00', '21:00', '22:00', '23:00', '00:00', '01:00', '02:00', '03:00','04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00',],
+            arr: [],
+        };
+    }
+
+    createShift(payload) {
+        const arr = [];
+
+        for (let i = 0; i < 15; i++) {
+            if (i === 0) {
+                arr.push( <Hour first={null} second={null}></Hour>)
+             }
+             arr.push(<Hour first={i === 0 ?payload[i] : null} second={payload[i + 1]}></Hour>);
+        }
+
+        payload.forEach((ele, i) => {
+           
+        });
+        return arr;
+    }
+
     render() {
         return (
             <div id={s.TL_Wrapper}>
