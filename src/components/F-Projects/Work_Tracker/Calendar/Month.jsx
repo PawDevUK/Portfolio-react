ada7edaa9cdae3908fe7b62cecefe4dd31fa567a
GitcrackerUK <pfsiwek@gmail.com>
2022-09-21 22:16:28 +0100

    created initial Month component

---
+++ src/components/F-Projects/Work_Tracker/Calendar/Month.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-component';
+import React, { Component } from 'react'
+
+const Wrapper = styled.div`
+    height:196px;
+    width:225px;
+    background-color: #373435;
+`
+
+export default class Month extends Component {
+  render() {
+    return (
+      <Wrapper>
+
+      </Wrapper>
+    )
+  }
+}
