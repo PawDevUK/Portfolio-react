4ee8c4adee7920af9dc133354fa7c7ac61fb61ce
GitcrackerUK <pfsiwek@gmail.com>
2022-09-21 23:21:47 +0100

    formatting

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,11 +1,8 @@
-
-export default function Visitors(state = 0 , action){
-    switch(action.type){
+export default function Visitors(state = 0, action) {
+    switch (action.type) {
         case 'ADD_COUNTER':
-            
-            return action.payload
-          
-            default:
-                return state
+            return action.payload;
+        default:
+            return state;
     }
 }
