08124613202f3fd9a43010b0149e8cda9a823278
GitcrackerUK <pfsiwek@gmail.com>
2022-09-21 14:26:17 +0100

    added division of the DayTime component and imported DayTimeLine component

--- src/components/F-Projects/Work_Tracker/dayTime/DayTime.jsx
+++ src/components/F-Projects/Work_Tracker/dayTime/DayTime.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import s from '../style/style.module.css';
+import DayTimeLine from './DayTimeLine';
 import Eclipse from './Eclipse';
 
 export default class DayTime extends Component {
     render() {
         return (
             <div id={s.TL_Wrapper}>
-                <div className={s.main}></div>
+                <div className={s.main}>
+                    <div className={s.mainTop}></div>
+                    <div className={s.mainBottom}>
+                        <DayTimeLine></DayTimeLine>
+                    </div>
+                   
+                </div>
                 <div id={s.right}>
                     <Eclipse basic={100} overTime={100}></Eclipse>
                 </div>
