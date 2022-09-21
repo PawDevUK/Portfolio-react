1cfdecf830c40834373c6a75fa966cf1794187bf
GitcrackerUK <pfsiwek@gmail.com>
2022-09-22 00:08:51 +0100

    added redux state to component and switch for routes

--- src/components/F-Projects/Work_Tracker/Work_Tracker.jsx
+++ src/components/F-Projects/Work_Tracker/Work_Tracker.jsx
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 
+import Calendar from './Calendar/Calendar.jsx';
 import DayTime from './DayTime/DayTime.jsx';
 import Menu from './Menu/Menu';
+import { connect } from "react-redux";
 import s from './style/style.module.css';
 
-export default class WorkTracker extends Component {
+class WorkTracker extends Component {
+
+    returnRoute(route){
+        let payload = ''
+        switch(route) {
+            case 'Calendar':
+                payload = <Calendar></Calendar>
+                break;
+            case 'Day Tracker':
+                payload = <DayTime></DayTime> 
+              break;
+            default:
+
+          }
+          return payload
+    }
+    
     render() {
         return (
             <div id={s.wrapper}>
                 <Menu>
-                    <DayTime></DayTime>
+                    {this.returnRoute(this.props.WT_Route)}
                 </Menu>
             </div>
         );
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        WT_Route:state.WT_Route
+    }
+}
+export default connect(mapStateToProps)(WorkTracker)
