41a4eaa2b07d3f9179c3b3a90fb94c8fb82faf88
GitcrackerUK <pfsiwek@gmail.com>
2023-02-26 21:51:18 +0000

    added classes

--- src/components/I-CV/cv.jsx
+++ src/components/I-CV/cv.jsx
@@ -43,15 +43,15 @@ function Cv() {
               <div className="head">languages:</div>
             </div>
             <div id="right">
-              <div className="">P.F.Siwek@gmail.com</div>
-              <div className="">Reading, UK</div>
-              <div className="">07463765514</div>
-              <div className="">
+              <div className="info">P.F.Siwek@gmail.com</div>
+              <div className="info">Reading, UK</div>
+              <div className="info">07463765514</div>
+              <div className="info">
                 <a href="http://www.pawelsiwek.co.uk">pawelsiwek.co.uk</a>
               </div>
               <ul>
-                <li>English-fluent</li>
-                <li>Polish-native</li>
+                <li>English (fluent)</li>
+                <li>Polish (native)</li>
               </ul>
             </div>
           </article>
@@ -131,17 +131,17 @@ function Cv() {
             <h4>Experience</h4>
           </header>
           <article id="art-work">
-            <h5>
+            <div class='cv-h5'>
               <span>Portfolio</span>
-            </h5>
+            </div>
             <p>
               Website created with use of React, create-react-app,
               react-bootstrap, react-router-dom, styled-components, material-ui
               and few more.
             </p>
-            <h5>
+            <div class='cv-h5'>
               <span>Bot Chat</span>
-            </h5>
+            </div>
             <p>
               Bot Chat is a simple app allowing any one to have conversation
               with AI. <br /> It’s simple and fun. You can type what ever you
@@ -155,29 +155,29 @@ function Cv() {
               BotLibre API, React, Create-React-App, React-Router-Dom, Axios,
               XML2JS, Gcloud
             </p>
-            <h5>
+            <div class='cv-h5'>
               <span>Fitness App</span>
-            </h5>
+            </div>
             <p>
               Very simple fitness app to track exercises duration, date. App is
               created in React/Node.js and deployed on Gcloud. Database used to
               storage inputs is MongoDb Atlas.
             </p>
-            <h5>
+            <div class='cv-h5'>
               <span>Nasa website</span>
-            </h5>
+            </div>
             <p>
               Webpage created in Vanilla Js and Bootstrap for RWD, dedicated to
               space related topics. All data presented on page is fetched from
               Nasa via Nasa API
             </p>
-            <h5>
+            <div class='cv-h5'>
               <span>Gentleman Barber</span>
-            </h5>
+            </div>
             <p>Simple barber shop website.</p>
-            <h5>
+            <div class='cv-h5'>
               <span>Goodwood Solicitors</span>
-            </h5>
+            </div>
             <p>Mobile-first webpage with menu to sub-pages.</p>
           </article>
         </section>
@@ -189,10 +189,10 @@ function Cv() {
             <h4>Education</h4>
           </header>
           <article>
-            <h5>
+            <div class='cv-h5'>
               <span>Relevant Coursework</span>
               <time></time>
-            </h5>
+            </div>
             <p>Complete React Developer in 2021 (w/ Redux, Hooks, GraphQL)</p>
             <p>JavaScript: Understanding the Weird Parts</p>
             <p>The Modern Javascript Bootcamp Course 2020</p>
@@ -202,24 +202,24 @@ function Cv() {
             </p>
             <p>Figma - UI/UX Design</p>
 
-            <h5>
+            <div class='cv-h5'>
               <span>Python backend bootcamp</span>
               <time>January 2019-February 2019</time>
-            </h5>
+            </div>
 
             <p>
               Four weeks Bootcamp focused on backend with:<br></br>Python,
               Flask, Django also HTML, CSS, JavaScript, <br /> Github and
               Testing.
             </p>
-            <h5>
+            <div class='cv-h5'>
               <span>Technical School of Mechanical Engineering</span>
               <time>2003 - 2004</time>
-            </h5>
-            <h5>
+            </div>
+            <div class='cv-h5'>
               <span>Basic Vocational School</span>
               <time> 2000 - 2003 </time>
-            </h5>
+            </div>
           </article>
         </section>
       </div>
