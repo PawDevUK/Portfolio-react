db211a72ca57db5b2b6554339c7846a8218a3ecc
GitcrackerUK <pfsiwek@gmail.com>
2023-05-28 21:04:33 +0100

    new component

---
+++ src/components/F-Projects/Work_Tracker/login-register/right/RightWrapper.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    width:205px;
+    height:280px;
+    background-color: fff;
+`;
+
+export default class RightWrapper extends Component {
+  render() {
+    return (
+      <Wrapper>
+        
+      </Wrapper>
+    )
+  }
+}
