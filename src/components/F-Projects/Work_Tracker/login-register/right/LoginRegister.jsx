8651ac3ff6c68bef88a0a865cd1785a316eff639
GitcrackerUK <pfsiwek@gmail.com>
2023-05-28 21:02:59 +0100

    new component

---
+++ src/components/F-Projects/Work_Tracker/login-register/right/LoginRegister.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import Login from './Login'
+import Register from './Register'
+
+const route1 = 'register'
+
+export default class LoginRegister extends Component {
+  render() {
+    return (
+      <div>Login-Register</div>
+    )
+  }
+}
