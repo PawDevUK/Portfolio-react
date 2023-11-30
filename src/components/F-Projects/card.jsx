b19fe410e8883ddaf12e0c1c613b927ddd46501c
pawel <pfsiwek@gmail.com>
2023-11-30 13:36:34 +0100

    fixed sizing issues on different screen sizes

--- src/components/F-Projects/card.jsx
+++ src/components/F-Projects/card.jsx
@@ -1,64 +1,89 @@
 import { getPLineHight } from 'selectors/pageContent.selector'
+import {expand} from 'img'
 import Button from 'components/common/Button'
 import styled from 'styled-components'
 import { boxShadow12 } from 'styled.js'
 import { connect } from 'react-redux'
 import { LimitText } from 'factory'
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import { cardTextLength } from 'factory'
 import Stack from './Stack'
 
-const Header = styled.h5`
-  font-size:18px;
-  margin:8px 0px 4px 0px;
-  position:relative;
-  display:inline-block;
-`
 const Underline = styled.div`
   position: absolute;
-  height:3px;
+  height:1px;
   width:100%;
   opacity:0;
-  background:#00000070;
+  background:#172a3f96;
   border-radius:5px;
   left:0px;
   bottom:-1px;
-  transition: opacity 0.3s ease
-`
+  transition: opacity 0.3s ease;
+  `
 const Wrapper = styled.div`
+  /* To push inner wrapper all the way down */
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  /* ------- */
   z-index:1000;
   background-color:#fff;
   border:solid 1px #dbdbdb;
   margin:20px auto 0px auto;
   padding:4px;
   border-radius:10px;
-  cursor: pointer;
   ${boxShadow12};
   width:65%;
   @media (max-width:600px) {
     width:60%;
   }
   @media (min-width:992px) {
-    height:430px;
     width:45%;
   }
   &:hover ${Underline}{
     opacity:1;
   }
+  `
+const HeaderWrapper = styled.div`
+  margin:0px auto 5px auto;
+  width:100%;
+  display:flex;
+  position:relative;
+  `
+  const TitleWrapper = styled.div`
+    width: 100%;
+    text-align: center;
+  `
+  const Header = styled.h5`
+    font-size:24px;
+    margin:8px 0px 4px 0px;
+    position:relative;
+    display:inline-block;
+  `
+const Expand = styled.div`
+  margin:0 0 0 auto;
+  background-image: url(${expand});
+  background-size: cover;
+  background-position: center;
+  height:30px;
+  width:30px;
 `
-const CardContent = styled.div`
+const TextWrapper = styled.div`
+  padding:5px;
   z-index:10000;
   text-align:center;
-  margin:0 10px 10px 10px;
+  margin: 5px 20px;
 `
 const StImg = styled.img`
+
   display:block;
   margin:auto;
-  height:200px;
+  /* height:200px; */
   width:80%;
 `
 const ButtonWrapper = styled.div`
   justify-content:space-between;
-  margin:10px auto auto auto;
+  margin: auto auto 10px auto;
   display:flex;
   width:250px;
   @media (max-width:600px){
@@ -77,35 +102,54 @@ const A = styled.a`
   }
 `
  function Card({ ...props }) {
+
+  const [textLength, setTextLength] = useState(0)
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+      window.addEventListener('resize', ()=> setWidth(
+          window.innerWidth
+      ))
+      setTextLength(cardTextLength(width));
+  },[width])
+
   return (
     <Wrapper>
-      <Stack stack={props.stack}></Stack>
+      <HeaderWrapper>
+        <TitleWrapper>
+          <Header> 
+              {props.item.title}
+              <Underline></Underline>
+          </Header>
+        </TitleWrapper>
+        {/* <TopBtnWrapper>
+          <Expand></Expand>
+        </TopBtnWrapper> */}
+      </HeaderWrapper>
+      {width>=650?<Stack stack={props.stack}></Stack>:null}
       <StImg 
         src={props.item.src} 
-        alt={props.item.alt}></StImg>
-      <CardContent>
-        <Header> 
-          {props.item.title}
-          <Underline></Underline>
-        </Header>
+        alt={props.item.alt}>
+      </StImg>
+      <TextWrapper>
         <P 
           p_line_height={props.p_line_height} 
         >
-            {LimitText(props.item.text, 130)}
+            {LimitText(props.item.text, !textLength?150:textLength)}
         </P>
-        <ButtonWrapper>
-          <A 
-            href={props.item.webHref.href} 
-            target="_blank">
-            <Button light>{props.item.webHref.button}</Button>
-          </A>
-          <A 
-            href={props.item.githubHref} 
-            target="_blank">
-            <Button light>Github Repo</Button>
-          </A>
-        </ButtonWrapper>
-      </CardContent>
+      </TextWrapper>
+      <ButtonWrapper>
+        <A 
+          href={props.item.webHref.href} 
+          target="_blank">
+          <Button light>{props.item.webHref.button}</Button>
+        </A>
+        <A 
+          href={props.item.githubHref} 
+          target="_blank">
+          <Button light>Github Repo</Button>
+        </A>
+      </ButtonWrapper>
     </Wrapper>
   )
 }
