00633178d327d3d6183b74e986d767d27b0d3ed2
pawel <pfsiwek@gmail.com>
2023-12-03 22:46:30 +0100

    json object to fill game stats

--- src/config/TicTacToe.config.js
+++ src/config/TicTacToe.config.js
@@ -1,274 +1,303 @@
 
 export const users = [
-    {
-      "name": "Karen19",
-      "status": "online",
-      "playingWith": "John9",
-      "playedGames": 1334,
-      "wonGames": 281,
-      "lostGames": 1053,
-      "ranking": 25
-    },
-    {
-      "name": "John9",
-      "status": "online",
-      "playingWith": "Michael42",
-      "playedGames": 1284,
-      "wonGames": 1172,
-      "lostGames": 112,
-      "ranking": 8
-    },
-    {
-      "name": "Michael77",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 481,
-      "wonGames": 50,
-      "lostGames": 431,
-      "ranking": 29
-    },
-    {
-      "name": "Susan88",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 580,
-      "wonGames": 664,
-      "lostGames": -84,
-      "ranking": 19
-    },
-    {
-      "name": "Lisa9",
-      "status": "online",
-      "playingWith": "John9",
-      "playedGames": 618,
-      "wonGames": 356,
-      "lostGames": 262,
-      "ranking": 24
-    },
-    {
-      "name": "Michael69",
-      "status": "online",
-      "playingWith": "David61",
-      "playedGames": 729,
-      "wonGames": 229,
-      "lostGames": 500,
-      "ranking": 27
-    },
-    {
-      "name": "Robert14",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 475,
-      "wonGames": 881,
-      "lostGames": -406,
-      "ranking": 13
-    },
-    {
-      "name": "Susan39",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 158,
-      "wonGames": 842,
-      "lostGames": -684,
-      "ranking": 16
-    },
-    {
-      "name": "Michael70",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 664,
-      "wonGames": 466,
-      "lostGames": 198,
-      "ranking": 22
-    },
-    {
-      "name": "Jennifer75",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 844,
-      "wonGames": 49,
-      "lostGames": 795,
-      "ranking": 30
-    },
-    {
-      "name": "David37",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 703,
-      "wonGames": 1381,
-      "lostGames": -678,
-      "ranking": 4
-    },
-    {
-      "name": "Lisa68",
-      "status": "online",
-      "playingWith": "Karen19",
-      "playedGames": 1018,
-      "wonGames": 1439,
-      "lostGames": -421,
-      "ranking": 1
-    },
-    {
-      "name": "Jennifer30",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 317,
-      "wonGames": 224,
-      "lostGames": 93,
-      "ranking": 28
-    },
-    {
-      "name": "Susan9",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 509,
-      "wonGames": 729,
-      "lostGames": -220,
-      "ranking": 17
-    },
-    {
-      "name": "David41",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 1129,
-      "wonGames": 931,
-      "lostGames": 198,
-      "ranking": 11
-    },
-    {
-      "name": "Michael56",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 1214,
-      "wonGames": 451,
-      "lostGames": 763,
-      "ranking": 23
-    },
-    {
-      "name": "David61",
-      "status": "online",
-      "playingWith": "Karen66",
-      "playedGames": 630,
-      "wonGames": 1325,
-      "lostGames": -695,
-      "ranking": 5
-    },
-    {
-      "name": "David86",
-      "status": "online",
-      "playingWith": "David61",
-      "playedGames": 1479,
-      "wonGames": 913,
-      "lostGames": 566,
-      "ranking": 12
-    },
-    {
-      "name": "Jennifer53",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 210,
-      "wonGames": 1428,
-      "lostGames": -1218,
-      "ranking": 2
-    },
-    {
-      "name": "Susan67",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 1392,
-      "wonGames": 1262,
-      "lostGames": 130,
-      "ranking": 6
-    },
-    {
-      "name": "Karen66",
-      "status": "online",
-      "playingWith": "Michael42",
-      "playedGames": 1192,
-      "wonGames": 711,
-      "lostGames": 481,
-      "ranking": 18
-    },
-    {
-      "name": "Lisa71",
-      "status": "online",
-      "playingWith": "Lisa9",
-      "playedGames": 376,
-      "wonGames": 876,
-      "lostGames": -500,
-      "ranking": 14
-    },
-    {
-      "name": "Karen78",
-      "status": "online",
-      "playingWith": "Lisa71",
-      "playedGames": 1181,
-      "wonGames": 1055,
-      "lostGames": 126,
-      "ranking": 10
-    },
-    {
-      "name": "John70",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 214,
-      "wonGames": 550,
-      "lostGames": -336,
-      "ranking": 20
-    },
-    {
-      "name": "Lisa1",
-      "status": "online",
-      "playingWith": "Michael42",
-      "playedGames": 1247,
-      "wonGames": 1112,
-      "lostGames": 135,
-      "ranking": 9
-    },
-    {
-      "name": "James23",
-      "status": "online",
-      "playingWith": "Lisa1",
-      "playedGames": 1023,
-      "wonGames": 1386,
-      "lostGames": -363,
-      "ranking": 3
-    },
-    {
-      "name": "John86",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 452,
-      "wonGames": 528,
-      "lostGames": -76,
-      "ranking": 21
-    },
-    {
-      "name": "Jennifer48",
-      "status": "offline",
-      "playingWith": null,
-      "playedGames": 1257,
-      "wonGames": 872,
-      "lostGames": 385,
-      "ranking": 15
-    },
-    {
-      "name": "Michael42",
-      "status": "online",
-      "playingWith": "David86",
-      "playedGames": 143,
-      "wonGames": 1238,
-      "lostGames": -1095,
-      "ranking": 7
-    },
-    {
-      "name": "Karen37",
-      "status": "online",
-      "playingWith": "David61",
-      "playedGames": 1325,
-      "wonGames": 257,
-      "lostGames": 1068,
-      "ranking": 26
-    }
-  ]
-  
+  {
+    "name": "Lisa60",
+    "status": "online",
+    "playingWith": "James97",
+    "playedGames": 912,
+    "wonGames": 572,
+    "lostGames": 340,
+    "ranking": 15,
+    "id": 8639
+  },
+  {
+    "name": "Mary43",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 33,
+    "wonGames": 5,
+    "lostGames": 28,
+    "ranking": 9,
+    "id": 2448
+  },
+  {
+    "name": "Mary30",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 918,
+    "wonGames": 520,
+    "lostGames": 398,
+    "ranking": 18,
+    "id": 2288
+  },
+  {
+    "name": "Lisa82",
+    "status": "online",
+    "playingWith": "Michael85",
+    "playedGames": 826,
+    "wonGames": 445,
+    "lostGames": 381,
+    "ranking": 8,
+    "id": 7572
+  },
+  {
+    "name": "Susan58",
+    "status": "online",
+    "playingWith": "John4",
+    "playedGames": 1124,
+    "wonGames": 181,
+    "lostGames": 943,
+    "ranking": 9,
+    "id": 3985
+  },
+  {
+    "name": "Jennifer21",
+    "status": "online",
+    "playingWith": "Robert67",
+    "playedGames": 896,
+    "wonGames": 218,
+    "lostGames": 678,
+    "ranking": 4,
+    "id": 1701
+  },
+  {
+    "name": "Susan94",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 392,
+    "wonGames": 16,
+    "lostGames": 376,
+    "ranking": 15,
+    "id": 1484
+  },
+  {
+    "name": "Karen18",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 1333,
+    "wonGames": 802,
+    "lostGames": 531,
+    "ranking": 13,
+    "id": 3188
+  },
+  {
+    "name": "Michael99",
+    "status": "online",
+    "playingWith": "Jennifer83",
+    "playedGames": 1311,
+    "wonGames": 124,
+    "lostGames": 1187,
+    "ranking": 14,
+    "id": 7846
+  },
+  {
+    "name": "Susan7",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 13,
+    "wonGames": 10,
+    "lostGames": 3,
+    "ranking": 2,
+    "id": 2309
+  },
+  {
+    "name": "Jennifer35",
+    "status": "online",
+    "playingWith": "Michael44",
+    "playedGames": 747,
+    "wonGames": 130,
+    "lostGames": 617,
+    "ranking": 10,
+    "id": 3323
+  },
+  {
+    "name": "Jennifer99",
+    "status": "online",
+    "playingWith": "John15",
+    "playedGames": 22,
+    "wonGames": 8,
+    "lostGames": 14,
+    "ranking": 3,
+    "id": 5626
+  },
+  {
+    "name": "James87",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 1392,
+    "wonGames": 289,
+    "lostGames": 1103,
+    "ranking": 19,
+    "id": 2497
+  },
+  {
+    "name": "Susan24",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 382,
+    "wonGames": 280,
+    "lostGames": 102,
+    "ranking": 17,
+    "id": 8334
+  },
+  {
+    "name": "Susan62",
+    "status": "online",
+    "playingWith": "John7",
+    "playedGames": 716,
+    "wonGames": 426,
+    "lostGames": 290,
+    "ranking": 4,
+    "id": 3916
+  },
+  {
+    "name": "Jennifer64",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 169,
+    "wonGames": 14,
+    "lostGames": 155,
+    "ranking": 13,
+    "id": 5683
+  },
+  {
+    "name": "Michael61",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 639,
+    "wonGames": 420,
+    "lostGames": 219,
+    "ranking": 19,
+    "id": 2269
+  },
+  {
+    "name": "Jennifer34",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 607,
+    "wonGames": 542,
+    "lostGames": 65,
+    "ranking": 8,
+    "id": 1317
+  },
+  {
+    "name": "Karen28",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 101,
+    "wonGames": 99,
+    "lostGames": 2,
+    "ranking": 16,
+    "id": 5200
+  },
+  {
+    "name": "Lisa35",
+    "status": "online",
+    "playingWith": "Robert92",
+    "playedGames": 1016,
+    "wonGames": 458,
+    "lostGames": 558,
+    "ranking": 10,
+    "id": 1178
+  },
+  {
+    "name": "Robert42",
+    "status": "online",
+    "playingWith": "Susan43",
+    "playedGames": 54,
+    "wonGames": 23,
+    "lostGames": 31,
+    "ranking": 21,
+    "id": 9955
+  },
+  {
+    "name": "James28",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 534,
+    "wonGames": 448,
+    "lostGames": 86,
+    "ranking": 28,
+    "id": 3212
+  },
+  {
+    "name": "Susan81",
+    "status": "online",
+    "playingWith": "John53",
+    "playedGames": 1423,
+    "wonGames": 363,
+    "lostGames": 1060,
+    "ranking": 9,
+    "id": 9104
+  },
+  {
+    "name": "Mary97",
+    "status": "online",
+    "playingWith": "Robert16",
+    "playedGames": 281,
+    "wonGames": 194,
+    "lostGames": 87,
+    "ranking": 12,
+    "id": 3151
+  },
+  {
+    "name": "Jennifer63",
+    "status": "online",
+    "playingWith": "John74",
+    "playedGames": 140,
+    "wonGames": 99,
+    "lostGames": 41,
+    "ranking": 9,
+    "id": 1133
+  },
+  {
+    "name": "Lisa28",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 92,
+    "wonGames": 16,
+    "lostGames": 76,
+    "ranking": 15,
+    "id": 2589
+  },
+  {
+    "name": "Robert94",
+    "status": "online",
+    "playingWith": "Mary90",
+    "playedGames": 272,
+    "wonGames": 201,
+    "lostGames": 71,
+    "ranking": 20,
+    "id": 5766
+  },
+  {
+    "name": "John77",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 1417,
+    "wonGames": 547,
+    "lostGames": 870,
+    "ranking": 20,
+    "id": 8873
+  },
+  {
+    "name": "Susan26",
+    "status": "offline",
+    "playingWith": null,
+    "playedGames": 1090,
+    "wonGames": 499,
+    "lostGames": 591,
+    "ranking": 9,
+    "id": 4004
+  },
+  {
+    "name": "Jennifer9",
+    "status": "online",
+    "playingWith": "Michael90",
+    "playedGames": 1164,
+    "wonGames": 1103,
+    "lostGames": 61,
+    "ranking": 25,
+    "id": 5235
+  }
+]
