4ee8d6935dd8e00714857006d0bcda4fc9e6dd92
pawel <pfsiwek@gmail.com>
2023-12-03 22:45:29 +0100

    imported dummy data and added fields

--- src/components/F-Projects/TicTacToe/components/PlayerList.jsx
+++ src/components/F-Projects/TicTacToe/components/PlayerList.jsx
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button';
 import { getUsers } from '../API/routes';
 import { boxShadow3 } from '../styled';
 import styled from 'styled-components';
+import {users} from 'config/TicTacToe.config'
 
 const Wrapper = styled.div`
     background-color: #575a5f47;
@@ -15,21 +16,11 @@ const Wrapper = styled.div`
 `;
 
 const columns = [
-    { field: 'user', headerName: 'User', width: 100 },
+    { field: 'ranking', headerName: 'Ranking', width: 120 },
+    { field: 'name', headerName: 'User', width: 120 },
     { field: 'status', headerName: 'Status', width: 130 },
-    { field: 'playingWith', headerName: 'Playing with', width: 130 },
-    {
-        field: 'action',
-        headerName: 'Action',
-        width: 130,
-        renderCell: (params) => (
-            <strong>
-                <Button variant='contained' color='primary' size='small' style={{ marginLeft: 16 }}>
-                    {params.row.status === 'Available' ? 'Join' : 'Watch'}
-                </Button>
-            </strong>
-        ),
-    },
+    { field: 'playingWith', headerName: 'Playing with', width: 150 },
+    { field: 'playedGames', headerName: 'Played Games', width: 120 },
 ];
 
 export default function PlayerList(params) {
@@ -40,15 +31,19 @@ export default function PlayerList(params) {
         setOnlineUsers(onlineUsers);
     }
 
-    useEffect(() => {
-        getUsersFromDB();
-        const interval = setInterval(() => {
-            getUsersFromDB();
-        }, 5000);
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
+    // useEffect(() => {
+    //     getUsersFromDB();
+    //     const interval = setInterval(() => {
+    //         getUsersFromDB();
+    //     }, 5000);
+    //     return () => {
+    //         clearInterval(interval);
+    //     };
+    // }, []);
+
+    useEffect(()=>{
+        setOnlineUsers(users);
+    },[])
 
     return (
         <Wrapper>
