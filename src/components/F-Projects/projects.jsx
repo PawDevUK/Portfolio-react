2efcea8bb3094e079f8f177696b686e5644cc752
pawel <pfsiwek@gmail.com>
2023-12-03 20:04:12 +0100

    Added requirements for new functionality.

--- src/components/F-Projects/projects.jsx
+++ src/components/F-Projects/projects.jsx
@@ -6,6 +6,12 @@ import { projects } from 'config/projects.config'
 import Card from './card'
 import AOS from 'aos'
 
+//  Add "enlarge" card button to every card to top right corner.
+//  Add functionality to check which card is enlarged.
+//  Add functionality to send correct data to enlarged card.
+//  Create large card witch gets data of the project via props.
+//  
+
 const MainWrapper = styled.div`
   padding: 0px 0px 50px 0px;
   position: relative;
