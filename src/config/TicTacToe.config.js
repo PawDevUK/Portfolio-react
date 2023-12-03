5e77393ac05f3ecf55bc9f4e8f3640bc29b45f67
pawel <pfsiwek@gmail.com>
2023-12-03 20:59:20 +0100

    Created dummy json obj to fill up player table with data.

---
+++ src/config/TicTacToe.config.js
@@ -0,0 +1,274 @@
+
+export const users = [
+    {
+      "name": "Karen19",
+      "status": "online",
+      "playingWith": "John9",
+      "playedGames": 1334,
+      "wonGames": 281,
+      "lostGames": 1053,
+      "ranking": 25
+    },
+    {
+      "name": "John9",
+      "status": "online",
+      "playingWith": "Michael42",
+      "playedGames": 1284,
+      "wonGames": 1172,
+      "lostGames": 112,
+      "ranking": 8
+    },
+    {
+      "name": "Michael77",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 481,
+      "wonGames": 50,
+      "lostGames": 431,
+      "ranking": 29
+    },
+    {
+      "name": "Susan88",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 580,
+      "wonGames": 664,
+      "lostGames": -84,
+      "ranking": 19
+    },
+    {
+      "name": "Lisa9",
+      "status": "online",
+      "playingWith": "John9",
+      "playedGames": 618,
+      "wonGames": 356,
+      "lostGames": 262,
+      "ranking": 24
+    },
+    {
+      "name": "Michael69",
+      "status": "online",
+      "playingWith": "David61",
+      "playedGames": 729,
+      "wonGames": 229,
+      "lostGames": 500,
+      "ranking": 27
+    },
+    {
+      "name": "Robert14",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 475,
+      "wonGames": 881,
+      "lostGames": -406,
+      "ranking": 13
+    },
+    {
+      "name": "Susan39",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 158,
+      "wonGames": 842,
+      "lostGames": -684,
+      "ranking": 16
+    },
+    {
+      "name": "Michael70",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 664,
+      "wonGames": 466,
+      "lostGames": 198,
+      "ranking": 22
+    },
+    {
+      "name": "Jennifer75",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 844,
+      "wonGames": 49,
+      "lostGames": 795,
+      "ranking": 30
+    },
+    {
+      "name": "David37",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 703,
+      "wonGames": 1381,
+      "lostGames": -678,
+      "ranking": 4
+    },
+    {
+      "name": "Lisa68",
+      "status": "online",
+      "playingWith": "Karen19",
+      "playedGames": 1018,
+      "wonGames": 1439,
+      "lostGames": -421,
+      "ranking": 1
+    },
+    {
+      "name": "Jennifer30",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 317,
+      "wonGames": 224,
+      "lostGames": 93,
+      "ranking": 28
+    },
+    {
+      "name": "Susan9",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 509,
+      "wonGames": 729,
+      "lostGames": -220,
+      "ranking": 17
+    },
+    {
+      "name": "David41",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 1129,
+      "wonGames": 931,
+      "lostGames": 198,
+      "ranking": 11
+    },
+    {
+      "name": "Michael56",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 1214,
+      "wonGames": 451,
+      "lostGames": 763,
+      "ranking": 23
+    },
+    {
+      "name": "David61",
+      "status": "online",
+      "playingWith": "Karen66",
+      "playedGames": 630,
+      "wonGames": 1325,
+      "lostGames": -695,
+      "ranking": 5
+    },
+    {
+      "name": "David86",
+      "status": "online",
+      "playingWith": "David61",
+      "playedGames": 1479,
+      "wonGames": 913,
+      "lostGames": 566,
+      "ranking": 12
+    },
+    {
+      "name": "Jennifer53",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 210,
+      "wonGames": 1428,
+      "lostGames": -1218,
+      "ranking": 2
+    },
+    {
+      "name": "Susan67",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 1392,
+      "wonGames": 1262,
+      "lostGames": 130,
+      "ranking": 6
+    },
+    {
+      "name": "Karen66",
+      "status": "online",
+      "playingWith": "Michael42",
+      "playedGames": 1192,
+      "wonGames": 711,
+      "lostGames": 481,
+      "ranking": 18
+    },
+    {
+      "name": "Lisa71",
+      "status": "online",
+      "playingWith": "Lisa9",
+      "playedGames": 376,
+      "wonGames": 876,
+      "lostGames": -500,
+      "ranking": 14
+    },
+    {
+      "name": "Karen78",
+      "status": "online",
+      "playingWith": "Lisa71",
+      "playedGames": 1181,
+      "wonGames": 1055,
+      "lostGames": 126,
+      "ranking": 10
+    },
+    {
+      "name": "John70",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 214,
+      "wonGames": 550,
+      "lostGames": -336,
+      "ranking": 20
+    },
+    {
+      "name": "Lisa1",
+      "status": "online",
+      "playingWith": "Michael42",
+      "playedGames": 1247,
+      "wonGames": 1112,
+      "lostGames": 135,
+      "ranking": 9
+    },
+    {
+      "name": "James23",
+      "status": "online",
+      "playingWith": "Lisa1",
+      "playedGames": 1023,
+      "wonGames": 1386,
+      "lostGames": -363,
+      "ranking": 3
+    },
+    {
+      "name": "John86",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 452,
+      "wonGames": 528,
+      "lostGames": -76,
+      "ranking": 21
+    },
+    {
+      "name": "Jennifer48",
+      "status": "offline",
+      "playingWith": null,
+      "playedGames": 1257,
+      "wonGames": 872,
+      "lostGames": 385,
+      "ranking": 15
+    },
+    {
+      "name": "Michael42",
+      "status": "online",
+      "playingWith": "David86",
+      "playedGames": 143,
+      "wonGames": 1238,
+      "lostGames": -1095,
+      "ranking": 7
+    },
+    {
+      "name": "Karen37",
+      "status": "online",
+      "playingWith": "David61",
+      "playedGames": 1325,
+      "wonGames": 257,
+      "lostGames": 1068,
+      "ranking": 26
+    }
+  ]
+  
