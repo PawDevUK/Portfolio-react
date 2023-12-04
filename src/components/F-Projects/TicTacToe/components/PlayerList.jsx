006618008dc8d27c2efada6f16a8a518291a4ebf
pawel <pfsiwek@gmail.com>
2023-12-04 18:09:07 +0100

    Enlarged data grid and added new fields to display.

--- src/components/F-Projects/TicTacToe/components/PlayerList.jsx
+++ src/components/F-Projects/TicTacToe/components/PlayerList.jsx
@@ -9,18 +9,20 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
     background-color: #575a5f47;
     border-radius: 10px;
-    height: 300px;
-    width: 650px;
+    height: 600px;
+    width: 800px;
     margin: 30px;
     ${boxShadow3}
 `;
 
 const columns = [
-    { field: 'ranking', headerName: 'Ranking', width: 120 },
-    { field: 'name', headerName: 'User', width: 120 },
+    { field: 'ranking', headerName: 'Ranking', width: 129 },
+    { field: 'name', headerName: 'User', width: 130 },
     { field: 'status', headerName: 'Status', width: 130 },
-    { field: 'playingWith', headerName: 'Playing with', width: 150 },
-    { field: 'playedGames', headerName: 'Played Games', width: 120 },
+    { field: 'playingWith', headerName: 'Playing with', width: 160 },
+    { field: 'playedGames', headerName: 'Played Games', width: 170 },
+    { field: 'wonGames', headerName: 'Wins', width: 130 },
+    { field: 'lostGames', headerName: 'Lost', width: 130 },
 ];
 
 export default function PlayerList(params) {
