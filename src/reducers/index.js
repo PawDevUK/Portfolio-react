cd40ab22bd431c6d810011dee1c76d3a5156c4cb
pawel <pfsiwek@gmail.com>
2024-05-23 22:07:59 +0100

    imported and added particles options reducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 import EngLang from 'reducers/toggleLang.reducer'
 import PageContent from 'reducers/PageContent.reducer'
-import Users from 'reducers/users'
+import Users from 'reducers/users.reducer'
 import Visitors from 'reducers/counterReducer'
 import WT_Route from 'reducers/WT_router.reducer'
 import { combineReducers } from '@reduxjs/toolkit'
 import isChatOpen from 'reducers/chat.reducer'
+import ParticlesOptions from 'reducers/particlesOption.reducer'
 
 export default function rootReducer(){
     return combineReducers({
@@ -13,6 +14,7 @@ export default function rootReducer(){
         PageContent,
         isChatOpen,
         EngLang,
-        WT_Route
+        WT_Route,
+        ParticlesOptions
     })
 }
