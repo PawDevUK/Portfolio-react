c020a6df1e465aeb1ab8fde5172e5562a6eff3ce
pawel <pfsiwek@gmail.com>
2024-05-23 22:06:55 +0100

    particles config

---
+++ src/config/particles.config.js
@@ -0,0 +1,66 @@
+export const particlesOptions = {
+    fpsLimit: 60,
+    interactivity: {
+        events: {
+            onClick: {
+                enable: true,
+                mode: "push",
+            },
+            onHover: {
+                enable: true,
+                mode: "repulse",
+            },
+            resize: true,
+        },
+        modes: {
+            push: {
+                quantity: 4,
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
+            },
+        },
+    },
+    particles: {
+        color: {
+            value: "#ffffff",
+        },
+        links: {
+            color: "#ffffff",
+            distance: 100,
+            enable: true,
+            opacity: 0.5,
+            width: 0.2,
+        },
+        collisions: {
+            enable: true,
+        },
+        move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 2,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800,
+            },
+            value: 120,
+        },
+        opacity: {
+            value: 0.6,
+        },
+        shape: {
+            type: "circle",
+        },
+        size: {
+            random: true,
+            value: 2,
+        },
+    },
+    detectRetina: true,
+}
