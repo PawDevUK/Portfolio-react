1e8e518456f4e873cc9f286e72ea2d76da8f2e46
pawel <pfsiwek@gmail.com>
2024-09-10 21:14:09 +0100

    Changed content of the table

--- src/components/F-Projects/Work_Tracker/RotaTable/RotaTable.jsx
+++ src/components/F-Projects/Work_Tracker/RotaTable/RotaTable.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,53 +10,63 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(workYear, team, workingDays, daysOff, salary) {
+  return { workYear, team, workingDays, daysOff, salary };
 }
 
+const TableWrapper = styled.div`
+  margin-top:50px;
+`
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+createData('2021/2022', 'G', 210, 77, '£47.000'),
+createData('2022/2023', 'G', 210, 77, '£52.000'),
+createData('2023/2024', 'G', 210, 77, '£49.000'),
+createData('2023/2024', 'G', 210, 77, '£47.000'),
+  ];
 
-function RotaTable() {
+function RotaTable({ Edit }) {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align='right'>Calories</TableCell>
-            <TableCell align='right'>Fat&nbsp;(g)</TableCell>
-            <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
-            <TableCell align='right'>Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component='th' scope='row'>
-                {row.name}
-              </TableCell>
-              <TableCell align='right'>{row.calories}</TableCell>
-              <TableCell align='right'>{row.fat}</TableCell>
-              <TableCell align='right'>{row.carbs}</TableCell>
-              <TableCell align='right'>
-                <Button variant='outlined' endIcon={<EditIcon/>}>
-                  Edit
-                </Button>
-              </TableCell>
+    <TableWrapper>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Work Year</TableCell>
+              <TableCell align='center'>Team</TableCell>
+              <TableCell align='center'>Working Days</TableCell>
+              <TableCell align='center'>Days Off</TableCell>
+              <TableCell align='center'>Salary</TableCell>
+              <TableCell align='center'></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.WorkYear}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component='th' scope='row'>
+                  {row.workYear}
+                </TableCell>
+                <TableCell align='center' >{row.team}</TableCell>
+                <TableCell align='center'>{row.workingDays}</TableCell>
+                <TableCell align='center'>{row.daysOff}</TableCell>
+                <TableCell align='center'>{row.salary}</TableCell>
+                <TableCell align='center'>
+                  <Button
+                    variant='outlined'
+                    onClick={Edit}
+                    endIcon={<EditIcon />}
+                  >
+                    Edit
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </TableWrapper>
   );
 }
 
