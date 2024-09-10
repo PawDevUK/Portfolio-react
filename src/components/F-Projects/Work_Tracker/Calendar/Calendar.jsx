054d8adf5ffec7d5f6a39beec18c0c276f65c8c6
pawel <pfsiwek@gmail.com>
2024-09-10 21:13:00 +0100

    Moved editing roata to separate component RotaEdit

--- src/components/F-Projects/Work_Tracker/Calendar/Calendar.jsx
+++ src/components/F-Projects/Work_Tracker/Calendar/Calendar.jsx
@@ -1,110 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import { isSameDay } from 'date-fns';
-import './MyCalendar.css';
-import Switch from '@mui/material/Switch';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import SaveIcon from '@mui/icons-material/Save';
 import styled from 'styled-components';
-import moment from 'moment'
+import RotaTable from '../RotaTable/RotaTable';
+import RotaEdit from '../RotaEdit/RotaEdit';
 
-const CalendarWrapper = styled.div`
-  width: 90%;
-  margin: 0px 50px 50px 50px;
-  display: flex;
-  flex-wrap: wrap;
-`;
+const RotaWrapper = styled.div`
 
-const SwitchWrapper = styled.div``;
-const SwitchHeader = styled.h4``;
+`
 
-const ControlsWrapper = styled.div`
-  margin-top: 50px;
-  display: flex;
-`;
-const ButtonsWrapper = styled.div`
-  width: 200px;
-  margin: 20px;
-  display: flex;
-  justify-content: space-between;
-`;
 export default function MyCalendarComponent() {
-  const [selectedDates, setSelectedDates] = useState([]);
-  const [edit, setEdit] = useState(true);
 
-  const handleDayClick = (value) => {
-    setSelectedDates((prevState) => {
-      const isDateSelected = prevState.some((date) =>
-        isSameDay(date, value)
-      );
+  const [edit, setEdit] = useState(false);
 
-      if (isDateSelected) {
-        return prevState.filter(
-          (prevItem) => !isSameDay(prevItem, value)
-        );
-      } else {
-        return [...prevState, value];
-      }
-    });
-  };
 
-  const allowEdit = () => {
+  const handleEditClick = () => {
     setEdit((edit) => !edit);
   };
 
-  const isSelected = (date) => {
-    return selectedDates.some((selectedDate) => isSameDay(selectedDate, date));
-  };
-
-  const returnYear = () => {
-    let calendar = [];
-    for (let i = 0; i < 12; i++) {
-      const firstDayOfMonth = moment()
-        .startOf('year')
-        .add(i, 'months')
-        .toDate();
-      calendar.push(
-        <Calendar
-          value={firstDayOfMonth}
-          nextLabel={null}
-          prevLabel={null}
-          next2Label={null}
-          prev2Label={null}
-          calendarType='iso8601'
-          key={i}
-          onClickDay={edit ? handleDayClick : null}
-          tileClassName={({ date, view }) =>
-            view === 'month' && isSelected(date)
-              ? 'highlight react-calendar__tile--active'
-              : null
-          }
-        />
-      );
-    }
-    return calendar;
-  };
-
   return (
-    <div>
-      <ControlsWrapper>
-        <SwitchWrapper>
-          <SwitchHeader>Edit Rota</SwitchHeader>
-          <Switch
-            onChange={allowEdit}
-            checked={edit}
-            color='primary'
-            value='dynamic-class-name'
-          />
-        </SwitchWrapper>
-        <ButtonsWrapper>
-          <Button variant='outlined' endIcon={<SaveIcon />}>
-            Save
-          </Button>
-        </ButtonsWrapper>
-      </ControlsWrapper>
-      <CalendarWrapper>{returnYear()}</CalendarWrapper>
-    </div>
+    <RotaWrapper>
+      { edit ? <RotaEdit Edit={handleEditClick}  ></RotaEdit> : <RotaTable Edit={handleEditClick} ></RotaTable>}
+    </RotaWrapper>
   );
 }
--- src/components/F-Projects/Work_Tracker/RotaEdit/RotaEdit.jsx
+++ src/components/F-Projects/Work_Tracker/RotaEdit/RotaEdit.jsx
@@ -1,22 +1,99 @@
-import React from 'react'
+import React,{ useState } from 'react';
+import styled from 'styled-components';
+import Button from '@mui/material/Button';
+import 'react-calendar/dist/Calendar.css';
+import './MyCalendar.css';
+import Calendar from 'react-calendar';
 
+import SaveIcon from '@mui/icons-material/Save';
 
+import moment from 'moment';
+import { isSameDay } from 'date-fns';
 
+const CalendarWrapper = styled.div``;
 
-function RotaEdit() {
+const YearCalendarWrapper = styled.div`
+  width: 90%;
+  margin: 0px 50px 50px 50px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ControlsWrapper = styled.div`
+  margin-top: 50px;
+  display: flex;
+`;
+const ButtonsWrapper = styled.div`
+  width: 200px;
+  margin: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+function RotaEdit({Edit}) {
+  const [selectedDates, setSelectedDates] = useState([]);
+
+  const handleDayClick = (value) => {
+    setSelectedDates((prevState) => {
+      const isDateSelected = prevState.some((date) => isSameDay(date, value));
+
+      if (isDateSelected) {
+        return prevState.filter((prevItem) => !isSameDay(prevItem, value));
+      } else {
+        return [...prevState, value];
+      }
+    });
+  };
+
+  const isSelected = (date) => {
+    return selectedDates.some((selectedDate) => isSameDay(selectedDate, date));
+  };
+
+  const returnYear = () => {
+    let calendar = [];
+    function returnTaxYear(year){
+      // This function needs return dates between 1st of April of the year and 31 of March next year.
+      // Create array with 9 months of the year and 3 months of +1 year
+      
+      let taxYear = []
+    }
+    for (let i = 0; i < 12; i++) {
+      const firstDayOfMonth = moment()
+        .startOf('year')
+        .add(i, 'months')
+        .toDate();
+      calendar.push(
+        <Calendar
+          value={firstDayOfMonth}
+          nextLabel={null}
+          prevLabel={null}
+          next2Label={null}
+          prev2Label={null}
+          calendarType='iso8601'
+          key={i}
+          onClickDay={ handleDayClick}
+          tileClassName={({ date, view }) =>
+            view === 'month' && isSelected(date)
+              ? 'highlight react-calendar__tile--active'
+              : null
+          }
+        />
+      );
+    }
+    return calendar;
+  };
   return (
     <CalendarWrapper>
       <ControlsWrapper>
         <ButtonsWrapper>
-          <Button variant='outlined' endIcon={<SaveIcon />}>
+          <Button variant='outlined' onClick={Edit} endIcon={<SaveIcon />}>
             Save
           </Button>
         </ButtonsWrapper>
       </ControlsWrapper>
-      <RotaTable></RotaTable>
-      <YearCalendarWrapper>{edit?returnYear():null}</YearCalendarWrapper>
+      <YearCalendarWrapper>{returnYear()}</YearCalendarWrapper>
     </CalendarWrapper>
-  )
+  );
 }
 
-export default RotaEdit
+export default RotaEdit;
