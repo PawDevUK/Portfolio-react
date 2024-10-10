c6cfd0bbfaa12e1918de92abb0a4428e306ad3c3
pawel <pfsiwek@gmail.com>
2024-10-10 01:52:52 +0100

    Added clock header and body with styling.

--- src/components/F-Projects/Work_Tracker/Clock-in_out/Clock_IN_OUT.jsx
+++ src/components/F-Projects/Work_Tracker/Clock-in_out/Clock_IN_OUT.jsx
@@ -6,15 +6,35 @@ import moment from 'moment'
 const Wrapper = styled.div`
   margin-top: 50px;
   overflow: hidden; // Adding overflow property to prevent margin collapse
-  background: #dde3ea;
   width: 100%;
   height: 742px;
 `;
-const ClockWrapper= styled.div`
-    height: 205px;
-    width: 205px;
-    margin: 15px auto;
-`
+const ClockWrapper = styled.div`
+  height: 400px;
+  width: 240px;
+  margin: 15px auto;
+`;
+const ClockHeader = styled.div`
+  color:#1976d2;
+  width: 240px;
+  height: 40px;
+  margin: 0px auto 5px auto;
+  border: solid 1px #1976d275;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const ClockBody = styled.div`
+  display: flex;
+  padding-top: 10px;
+  justify-content: center;
+  width: 240px;
+  height: 230px;
+  border: solid 1px #1976d275;
+  border-radius: 5px 5px 50% 50%;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08);
+`;
 const ClockBorder = styled.div`
   background-color: ${p=>p.faceColor};
   border-radius: 50%;
@@ -23,10 +43,12 @@ const ClockBorder = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1),
+              0 1px 3px rgba(0, 0, 0, 0.08);
+              `;
 
 const ClockFace= styled.div`
-  background-color: ${lightBlue};
+  background: linear-gradient(180deg, #1976d26b, #bbdefb);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -39,7 +61,7 @@ const ClockTimer = styled.div`
   width:100px;
   font-size:26px;
   font-weight: 500;
-  color:grey;
+  color:#fff;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -54,6 +76,7 @@ const TimerRecordsWrapper = styled.div`
   border: 2px solid #80808057;
   border-radius: 40px;
 `;
+
 
 export default function ClockInOut() {
     const [currentTime, setCurrentTime] = useState(moment().format('HH:mm:ss'));
@@ -76,14 +99,17 @@ function recordClockInOut() {
 }
   return (
     <Wrapper>
-      <ClockWrapper onClick={recordClockInOut}>
-        <ClockBorder faceColor={faceColor}>
+      <ClockWrapper>
+        <ClockHeader>{faceColor === `${greenTimer}` ? "Clock In" : "Clock Out" }</ClockHeader>
+        <ClockBody>
+        <ClockBorder faceColor={faceColor}  onClick={recordClockInOut}>
           <ClockFace >
             <ClockTimer>
                {currentTime}
             </ClockTimer>
           </ClockFace>
         </ClockBorder>
+        </ClockBody>
             <TimerRecordsWrapper>
 
             </TimerRecordsWrapper>
