76e1bcfb330b951cdad20e1dfd807caf39a31e32
pawel <pfsiwek@gmail.com>
2025-08-25 16:13:18 +0100

    Enlarged line-height for paragrphs.

--- src/components/common/typography.js
+++ src/components/common/typography.js
@@ -11,7 +11,7 @@ ${p=>p.dark?css`color:rgba(41, 87, 111, 0.46);`:css``}
 export const P = styled.p`
 ${p=>p.styleP?css`p.styleP`:css``};
 font-size: 16px;
-line-height: 20px;
+line-height: 24px;
 color: rgba(0, 0, 0, 0.6);
 ${p=>p.dark?css`
 color:#fff;
