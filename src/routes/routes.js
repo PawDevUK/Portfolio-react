8155ba0d8512cdbe1ae0308a4d3ada024db1e984
GitcrackerUK <pfsiwek@gmail.com>
2025-08-30 10:14:01 +0100

    Moved styles out of component.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,28 +12,18 @@ import ReactSection from '../components/E-React/React'
 import Projects from '../components/F-Projects/projects'
 import Footer from '../components/H-Footer/Footer.jsx'
 
-const WorkTracker = lazy( ()=>import('components/F-Projects/Work_Tracker/Work_Tracker') )
-const TicTacToe = lazy( ()=>import('../components/F-Projects/TicTacToe/TicTacToe') )
-const Login = lazy( ()=>import('../components/F-Projects/Register_Login/Login') )
-const Register = lazy( ()=>import('../components/F-Projects/Register_Login/Register') )
-const RegisterSignIn = lazy( ()=>import('../components/F-Projects/Register_Login/RegisterSignIn') )
-const Covid = lazy( ()=>import('../components/F-Projects/covid-app/Covid') )
+const WorkTracker = lazy(() => import('components/F-Projects/Work_Tracker/Work_Tracker'))
+const TicTacToe = lazy(() => import('../components/F-Projects/TicTacToe/TicTacToe'))
+const Login = lazy(() => import('../components/F-Projects/Register_Login/Login'))
+const Register = lazy(() => import('../components/F-Projects/Register_Login/Register'))
+const RegisterSignIn = lazy(() => import('../components/F-Projects/Register_Login/RegisterSignIn'))
+const Covid = lazy(() => import('../components/F-Projects/covid-app/Covid'))
 
-const CV = lazy( ()=>import('../components/I-CV/cv') )
-const Chat = lazy( ()=>import('../components/Z-Chat/Chat') )
+const CV = lazy(() => import('../components/I-CV/cv'))
+const Chat = lazy(() => import('../components/Z-Chat/Chat'))
 
-export default function Router(){
 
-const AboutRef = useRef(null);
 
-const scrollToElement = () => {
-  // Check if elementRef is valid
-  if (AboutRef.current) {
-    // Scroll to the element
-    AboutRef.current.scrollIntoView({ behavior: 'smooth' });
-  }
-};
-    
 const GlobalStyle = createGlobalStyle`
     body{
         background-color:#fff;
@@ -58,11 +48,24 @@ const Container = styled.div`
     margin: auto;
     padding: 0px;
 `
-    return(
-        <BrowserRouter basename='/'>
-        <Suspense fallback={<div></div>}>
+
+export default function Router() {
+
+  const AboutRef = useRef(null);
+
+  const scrollToElement = () => {
+    // Check if elementRef is valid
+    if (AboutRef.current) {
+      // Scroll to the element
+      AboutRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <BrowserRouter basename='/'>
+      <Suspense fallback={<div></div>}>
         <Wrapper>
-          <GlobalStyle/>
+          <GlobalStyle />
           <Switch>
             <Route exact path="/covid">
               <Covid></Covid>
@@ -104,7 +107,7 @@ const Container = styled.div`
             </Route>
           </Switch>
         </Wrapper>
-        </Suspense>
-      </BrowserRouter>
-    )
+      </Suspense>
+    </BrowserRouter>
+  )
 }
