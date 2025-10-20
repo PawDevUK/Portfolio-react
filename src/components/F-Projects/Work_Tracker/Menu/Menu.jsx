0f774a97037885fdaa4ee9607760c47c6a37a0da
GitcrackerUK <pfsiwek@gmail.com>
2025-10-20 21:31:52 +0100

    Formatting + added Presentation component.

--- src/components/F-Projects/Work_Tracker/Menu/Menu.jsx
+++ src/components/F-Projects/Work_Tracker/Menu/Menu.jsx
@@ -29,175 +29,179 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime';
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
 });
 
 const closedMixin = (theme) => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
 });
 
 const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    Menu,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  Menu,
 }));
 
 const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
+  }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-        ...openedMixin(theme),
-        '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-        ...closedMixin(theme),
-        '& .MuiDrawer-paper': closedMixin(theme),
-    }),
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
+  }),
 }));
 
 export default function Menu(props) {
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    const handleRoute = (route) => {
-        store.dispatch(WT_Route(route));
-    };
+  const handleRoute = (route) => {
+    store.dispatch(WT_Route(route));
+  };
 
-    const AddIconToTab = (text) => {
-      let payload = '';
-      switch (text) {
-        case `ClockInOut`:
-          payload = <AccessTimeIcon></AccessTimeIcon>;
-          break;
-        case 'Calendar':
-          payload = <CalendarMonthIcon></CalendarMonthIcon>;
-          break;
-        case 'Rota Picker':
-          payload = <DateRangeIcon></DateRangeIcon>;
-          break;
-        case 'Day Tracker':
-          payload = <InsertInvitationIcon></InsertInvitationIcon>;
-          break;
-        default:
-      }
-      return payload;
-    };
+  const AddIconToTab = (text) => {
+    let payload = '';
+    switch (text) {
+      case `ClockInOut`:
+        payload = <AccessTimeIcon></AccessTimeIcon>;
+        break;
+      case 'Calendar':
+        payload = <CalendarMonthIcon></CalendarMonthIcon>;
+        break;
+      case 'Presentation':
+        payload = <DateRangeIcon></DateRangeIcon>;
+        break;
+      case 'Day Tracker':
+        payload = <InsertInvitationIcon></InsertInvitationIcon>;
+        break;
+      default:
+    }
+    return payload;
+  };
 
-    const ReturnList = (arr) => {
-        return arr.map((text, index) => {
-            return (
-                <List>
-                    <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                        <ListItemButton
-                            onClick={() => {
-                                handleRoute(text);
-                            }}
-                            sx={{
-                                minHeight: 48,
-                                justifyContent: open ? 'initial' : 'center',
-                                px: 2.5,
-                            }}>
-                            <ListItemIcon
-                                sx={{
-                                    minWidth: 0,
-                                    mr: open ? 3 : 'auto',
-                                    justifyContent: 'center',
-                                }}>
-                                {AddIconToTab(text)}
-                            </ListItemIcon>
-                            <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-            );
-        });
-    };
-
-    return (
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position='fixed' open={open}>
-          <Toolbar>
-            <IconButton
-              color='inherit'
-              aria-label='open drawer'
-              onClick={handleDrawerOpen}
-              edge='start'
+  const ReturnList = (arr) => {
+    return arr.map((text, index) => {
+      return (
+        <List>
+          <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              onClick={() => {
+                handleRoute(text);
+              }}
               sx={{
-                marginRight: 5,
-                ...(open && { display: 'none' }),
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
               }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant='h6' noWrap component='div'>
-              Tesco Work Tracker
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant='permanent' open={open}>
-          <DrawerHeader>
-            <IconButton onClick={handleDrawerClose}>
-              {' '}
-              {theme.direction === 'rtl' ? (
-                <ChevronRightIcon />
-              ) : (
-                <ChevronLeftIcon />
-              )}
-            </IconButton>
-          </DrawerHeader>
-          <Divider />
-          {ReturnList(['ClockInOut', 'Calendar', 'Rota Picker', 'Day Tracker'])}
-          <Divider />
-        </Drawer>
-        <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-          {props.children}
-        </Box>
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {AddIconToTab(text)}
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      );
+    });
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position='fixed' open={open}>
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' noWrap component='div'>
+            Tesco Work Tracker
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant='permanent' open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {' '}
+            {theme.direction === 'rtl' ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        {ReturnList(['ClockInOut', 'Calendar', 'Presentation', 'Day Tracker'])}
+        <Divider />
+      </Drawer>
+      <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+        {props.children}
       </Box>
-    );
+    </Box>
+  );
 }
