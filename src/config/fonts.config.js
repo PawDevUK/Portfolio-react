af22c96713daced974975c6eaf78fe73c6dfa00e
GitcrackerUK <pfsiwek@gmail.com>
2025-10-23 20:49:36 +0100

    Reduced line-height.

--- src/config/fonts.config.js
+++ src/config/fonts.config.js
@@ -1,13 +1,13 @@
 export const fonts = {
-    size:{
-        small:'14px',
-        medium:'16px',
-        large:'18px',
-        x_large:'20px',
-        jumbo:'22px'
+    size: {
+        small: '14px',
+        medium: '16px',
+        large: '18px',
+        x_large: '20px',
+        jumbo: '22px'
     },
-    family:{
-        main:'Nunito Sans , sans-serif'
+    family: {
+        main: 'Nunito Sans , sans-serif'
     },
-    paragraph_line_height:'24'
+    paragraph_line_height: '22'
 }
