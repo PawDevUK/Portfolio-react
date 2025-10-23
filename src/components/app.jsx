831bf4dfa60bde3c51756d3956f0714539c51ebf
GitcrackerUK <pfsiwek@gmail.com>
2025-10-23 13:37:06 +0100

    Fixed Styles.css path.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,22 +1,21 @@
-
-import React, { useEffect } from 'react'
-import { store } from '../configureStore'
-import { Provider } from 'react-redux'
-import { syncConfigWithStore } from '../configureStore'
-import '../styles/themes.css'
-import '../../node_modules/bootstrap/dist/css/bootstrap.css'
-import Router from '../routes/routes'
-import 'styles/styles.css'
+import React, { useEffect } from 'react';
+import { store } from '../configureStore';
+import { Provider } from 'react-redux';
+import { syncConfigWithStore } from '../configureStore';
+import '../styles/themes.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import Router from '../routes/routes';
+import '../styles/styles.css';
 
 function App() {
-  useEffect(()=>{
+  useEffect(() => {
     syncConfigWithStore();
-  })
-    
+  });
+
   return (
     <Provider store={store}>
       <Router></Router>
     </Provider>
-  )
+  );
 }
-export default App
+export default App;
