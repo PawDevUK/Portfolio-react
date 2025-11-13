1f91e6a78a24e1a0a5d9ad550d469abdd7c32d6d
GitcrackerUK <pfsiwek@gmail.com>
2025-11-13 18:16:19 +0000

    Update SERVER_URL to use a hardcoded value instead of environment variable

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,2 @@
-import 'dotenv/config';
-const SERVER_URL = process.env.SERVER_BASE
+const SERVER_URL = 'https://portfolio-server-104qu2sac-pawdevs-projects-c1e9b938.vercel.app'
 export default SERVER_URL;
