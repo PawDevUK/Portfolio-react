29ef81734b77dc9afda6ec15dd5439fe24d3fd4e
GitcrackerUK <pfsiwek@gmail.com>
2025-11-20 11:40:12 +0000

    Refactor Toggler component to improve props handling and streamline styling

--- src/components/common/Toggler.jsx
+++ src/components/common/Toggler.jsx
@@ -1,68 +1,56 @@
-import React from 'react'
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Switch from '@material-ui/core/Switch';
-import {PolskaFlaga} from 'img/index'
 
-const Toggler = withStyles((theme) => ({
-  root: {
-    width: 42,
-    height: 26,
-    padding: 0,
-    margin: theme.spacing(1),
-  },
-  switchBase: {
-    height: 26,
-    padding: 1,
-    color: '#17293f',
-    '&$checked': {
-    color: '#17293f',
-    transform: 'translateX(16px)',
-      '& + $track': {
-        backgroundColor:'#fff',
-        backgroundImage: `url(${PolskaFlaga})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        opacity: 1,
-        border: 'none',
+const Toggler = ({ checkedImg, uncheckedImg, onChange, checked, ...props }) => {
+  const StyledSwitch = withStyles((theme) => ({
+    root: {
+      width: 42,
+      height: 26,
+      padding: 0,
+      margin: theme.spacing(1),
+    },
+    switchBase: {
+      height: 26,
+      padding: 1,
+      color: '#17293f',
+      '&$checked': {
+        color: '#17293f',
+        transform: 'translateX(16px)',
+        '& + $track': {
+          backgroundColor: '#fff',
+          backgroundImage: `url(${uncheckedImg})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          opacity: 1,
+          border: 'none',
+        },
       },
     },
-  },
-  thumb: {
-    width: 24,
-    height: 24,
-  },
-  track: {
-    borderRadius: 26 / 2,
-    backgroundImage: (props)=> `url(${props.checkedImg})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    opacity: 1,
-    transition: theme.transitions.create(['background-color', 'border']),
-  },
-  checked: {
-  },
-  focusVisible: {
-  }
-}))(({ classes, ...props }) => {
+    thumb: {
+      width: 24,
+      height: 24,
+    },
+    track: {
+      borderRadius: 26 / 2,
+      backgroundImage: `url(${checkedImg})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      opacity: 1,
+      transition: theme.transitions.create(['background-color', 'border']),
+    },
+    checked: {},
+    focusVisible: {},
+  }))(Switch);
+
   return (
-    <Switch
-      checkedImg={props.checkedImg}
-      uncheckedImg={props.uncheckedImg}
-      onChange={props.onChange}
-      checked={props.checked}
-      focusVisibleClassName={classes.focusVisible}
+    <StyledSwitch
+      onChange={onChange}
+      checked={checked}
       disableRipple
-      classes={{
-        root: classes.root,
-        switchBase: classes.switchBase,
-        thumb: classes.thumb,
-        track: classes.track,
-        checked: classes.checked,
-      }}
       {...props}
     />
   );
-});
+};
 
-
-export default Toggler
+export default Toggler;
