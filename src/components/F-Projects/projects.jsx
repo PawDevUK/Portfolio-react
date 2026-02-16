ab770fa1b345a99175726e4ef70bc9507892ebab
PawDevUK <p.f.siwek@gmail.com>
2026-02-16 11:28:58 +0000

    Added justify-content: center;

--- src/components/F-Projects/projects.jsx
+++ src/components/F-Projects/projects.jsx
@@ -1,42 +1,42 @@
-import JumboSectionHeader from 'components/common/JumboSectionHeader'
-import '../../../node_modules/aos/dist/aos.css'
-import styled from 'styled-components'
-import React, { useEffect } from 'react'
-import { projects } from 'config/projects.config'
-import Card from './card'
-import AOS from 'aos'
+import JumboSectionHeader from 'components/common/JumboSectionHeader';
+import '../../../node_modules/aos/dist/aos.css';
+import styled from 'styled-components';
+import React, { useEffect } from 'react';
+import { projects } from 'config/projects.config';
+import Card from './card';
+import AOS from 'aos';
 
 //  Add "enlarge" card button to every card to top right corner.
 //  Add functionality to check which card is enlarged.
 //  Add functionality to send correct data to enlarged card.
 //  Create large card witch gets data of the project via props.
-//  
+//
 
 const MainWrapper = styled.div`
   padding: 0px 0px 50px 0px;
   position: relative;
-`
+`;
 
 const Wrapper = styled.div`
   @media (max-width: 992px) {
     display: block;
   }
-  margin: auto;
   display: flex;
   flex-wrap: wrap;
-`
+  justify-content: center;
+`;
 export default function Projects() {
   useEffect(() => {
-    AOS.init({ duration: 1000 })
-  }, [])
+    AOS.init({ duration: 1000 });
+  }, []);
   return (
-    <MainWrapper id="projects" >
-      <JumboSectionHeader projects >Projects</JumboSectionHeader>
+    <MainWrapper id='projects'>
+      <JumboSectionHeader projects>Projects</JumboSectionHeader>
       <Wrapper>
         {projects.map((item, index) => {
-          return <Card stack={item.stack} key={index} item={item}></Card>
+          return <Card stack={item.stack} key={index} item={item}></Card>;
         })}
       </Wrapper>
     </MainWrapper>
-  )
+  );
 }
