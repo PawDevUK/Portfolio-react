979f8ab3f59ec577ea590484476b21268e71861d
PawDevUK <p.f.siwek@gmail.com>
2026-02-16 12:55:29 +0000

    Fix Header component styling: ensure bold font weight for consistency

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.h5`
   font-size: 20px;
+  font-weight: bold;
   margin: 8px 0px 4px 0px;
   position: relative;
   display: inline-block;
