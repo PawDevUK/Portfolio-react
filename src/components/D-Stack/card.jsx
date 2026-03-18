92eb48c936d72fa4c89182c0d6443e4f695ab81e
pawel <pfsiwek@gmail.com>
2026-03-18 09:36:25 +0000

    Added image container with height/width

--- src/components/D-Stack/card.jsx
+++ src/components/D-Stack/card.jsx
@@ -1,9 +1,9 @@
-import '../../../node_modules/aos/dist/aos.css'
+import '../../../node_modules/aos/dist/aos.css';
 
-import React, { useEffect } from 'react'
-import styled, { css } from 'styled-components'
+import React, { useEffect } from 'react';
+import styled, { css } from 'styled-components';
 
-import AOS from 'aos'
+import AOS from 'aos';
 
 const Wrapper = styled.div`
   @media (max-width: 700px) {
@@ -12,20 +12,19 @@ const Wrapper = styled.div`
     width: 130px;
   }
   margin: 5px;
-  height: 150px;
-  width: 150px;
-  box-shadow: 
+  height: 100px;
+  width: 100px;
+  box-shadow:
     0px 8px 10px rgba(0, 0, 0, 0.14),
     0px 3px 14px rgba(0, 0, 0, 0.12),
     0px 5px 5px rgba(0, 0, 0, 0.2);
   text-align: center;
   border-radius: 10px;
   padding: 1px;
-`
+`;
 const Text = styled.p`
-  margin: 10px auto 8px auto;
   font-size: 16px;
-`
+`;
 const Img = styled.img`
   @media (max-width: 700px) {
     ${(p) =>
@@ -39,27 +38,27 @@ const Img = styled.img`
             height: 80px;
           `};
   }
-  width: 122px;
-  height: 100px;
+  width: 100%;
+  height: 70%;
   object-fit: scale-down;
-`
+`;
 const Link = styled.a`
   &:hover {
     text-decoration: none;
   }
-`
+`;
 
 export default function MiniCard(props) {
   useEffect(() => {
     AOS.init({ duration: 1000 });
-  },[]);
+  }, []);
 
   return (
-    <Link href={props.href} target="_blank" data-aos="fade">
-        <Wrapper>
-          <Text>{props.title}</Text>
-          <Img styled={props.styled} src={props.image}></Img>
-        </Wrapper>
+    <Link href={props.href} target='_blank' data-aos='fade'>
+      <Wrapper>
+        <Text>{props.title}</Text>
+        <Img styled={props.styled} src={props.image}></Img>
+      </Wrapper>
     </Link>
-  )
+  );
 }
