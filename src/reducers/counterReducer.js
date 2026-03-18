e0719941d9de14162dd7b66c8aef3f4ad4079ba7
pawel <pfsiwek@gmail.com>
2026-03-18 09:38:42 +0000

    Added visitors counter value.

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,4 +1,4 @@
-export default function Visitors(state = 0, action) {
+export default function Visitors(state = 346, action) {
     switch (action.type) {
         case 'ADD_COUNTER':
             return action.payload;
